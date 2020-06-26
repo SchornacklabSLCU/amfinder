f28bf5d564b4dce0cf4f29e195a9befbaf9a110e
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-06-26 20:31:35 +0100

    Documentation

--- cGUI.mli
+++ cGUI.mli
@@ -7,9 +7,10 @@ val window : GWindow.window
 
 (** Horizontal toolbox (left pane). *)
 module HToolbox : sig
-  val toggles : (char * GButton.toggle_button) list 
-  val toggles_full : (char * (GButton.toggle_button * GMisc.image)) list
+  val toggles : (char * (GButton.toggle_button * GMisc.image)) array
+  (** Table of toggle buttons corresponding to the different annotations. *)
   val toggle_any : char -> unit
+  (** Inverts the status of the given toggle button. *)
 end
 
 
@@ -47,34 +48,26 @@ module VToolbox : sig
   val master : GButton.radio_tool_button * GMisc.image
   (** Main radio button. When active, all annotated tile get overlaid with a 
     * green square, no matter what the annotation is. *) 
-
   val radios : (char * (GButton.radio_tool_button * GMisc.image)) list
   (** Layer-specific radio buttons. When active, only tiles bearing the
-    * corresponding annotation are displayed. *)
-   
+    * corresponding annotation are displayed. *)  
   val row : GMisc.label
   (** Indicates the current row index. *)
-
   val column : GMisc.label
   (** Indicate the current column index. *)
-  
   val confidence : GMisc.label
   (** To be primarily used with computer-generated annotations. Indicates how
     * confident the annotation is (using percentage). *)
-  
   val confidence_color : GMisc.label
   (** To be primarily used with computer-generated annotations. This label
     * displays the Viridis colour corresponding to the percentage displayed by
     * the label [confidence] (see above). *)
-
   val get_active : unit -> [`CHR of char | `SPECIAL]
   (** Indicates which layer is currently active ([`SPECIAL] corresponds to
     * [master], while [`CHR _] corresponds to elements of the list [radios]
     * (see above). *)
-
   val export : GButton.tool_button
   (** Saves the current display as a PNG file (currently not implemented). *)
-  
   val preferences : GButton.tool_button
   (** Shows preferences dialog (currently not implemented). *)
 end
