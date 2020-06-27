90ee6681fa2d970e7ef907f0c2dadc411612832d
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-06-27 06:06:04 +0100

    Documentation

--- cEvent.mli
+++ cEvent.mli
@@ -1,5 +1,9 @@
 (* CastANet - cEvent.mli *)
 
+(** Event manager. *)
+
 val initialize : unit -> unit
+(** Initialize callback functions for all widgets. *)
 
 val update_annotations : GdkEvent.Key.t -> bool
+(** Change toggle button status based on the key pressed. *)
