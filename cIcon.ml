91d999a47464100657da7aaff2f26dff64651284
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-07-03 23:27:53 +0100

    Name changes

--- cIcon.ml
+++ cIcon.ml
@@ -12,11 +12,11 @@ let build_path_list suf =
   List.map (fun chr -> chr, path chr) CAnnot.code_list
 
 module Source = struct
-  let import n (c, s) = (c, GdkPixbuf.from_file_at_size ~width:n ~height:n s)
-  let import_multiple n = List.map (import n)
-  let get_any f = function `SMALL -> f 24 | `LARGE -> f 48
-  let get = get_any import
-  let get_multiple = get_any import_multiple
+  let pixbuf n (c, s) = (c, GdkPixbuf.from_file_at_size ~width:n ~height:n s)
+  let pixbuf_list n = List.map (pixbuf n)
+  let import f = function `SMALL -> f 24 | `LARGE -> f 48
+  let load = import pixbuf
+  let load_multiple = import pixbuf_list
 end
 
 module type IconSet = sig
@@ -27,8 +27,8 @@ end
 let icon_set suf =
   let module M = struct
     let names = build_path_list suf
-    let large = Source.get_multiple `LARGE names
-    let small = Source.get_multiple `SMALL names
+    let large = Source.load_multiple `LARGE names
+    let small = Source.load_multiple `SMALL names
   end in (module M : IconSet)
 
 let m_rgba = icon_set "rgba" (* Active toggle buttons.   *)
@@ -48,10 +48,10 @@ module Joker = struct
   let make suf = "*", Filename.concat dir (sprintf "Joker_%s.png" suf) 
   let rgba = make "rgba"
   let grey = make "grey"
-  let large_rgba = snd (Source.get `LARGE rgba)
-  let small_rgba = snd (Source.get `SMALL rgba)
-  let large_grey = snd (Source.get `LARGE grey)
-  let small_grey = snd (Source.get `SMALL grey)
+  let large_rgba = snd (Source.load `LARGE rgba)
+  let small_rgba = snd (Source.load `SMALL rgba)
+  let large_grey = snd (Source.load `LARGE grey)
+  let small_grey = snd (Source.load `SMALL grey)
 end
 
 let get_joker typ = function
