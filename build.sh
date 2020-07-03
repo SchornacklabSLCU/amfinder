7075c375a5aa7d0633a99afcd78ed9d7d5c245a5
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-07-03 06:01:18 +0100

    Switching to dune for project compilation

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
 #! /bin/bash
 
-BIN="castanet-editor"
-OPT='ocamlopt.opt'
-CFLAGS='-w,s,-g'
-OPT="ocamlopt -nodynlink -O3 -principal -inline 200"
-PKGS='lablgtk2,cairo2,cairo2-gtk'
-MLI="$(ocamldsort c*ml | sed 's/ml/mli/g')"
-ODIRS="-I +/../lablgtk2 -I +/../cairo2 -I +/../cairo2-gtk -I _build"
-
-eval `opam config env` && \
-ocamlbuild -cflags $CFLAGS -ocamlopt "$OPT" -pkgs $PKGS main.native && \
-ocamldoc $ODIRS -html -d html-doc -stars $MLI && \
-mv main.native "$BIN"
+dune build castanet_editor.exe && mv _build/4.08.0/castanet_editor.exe . 
