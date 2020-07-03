77a4759912d188bc0397a2b9c397e833d1dfc3c1
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-07-03 22:40:44 +0100

    Removing open Printf

--- cLog.ml
+++ cLog.ml
@@ -1,9 +1,7 @@
 (* CastANet - cLog.ml *)
 
-open Printf
-
 let message f cha typ fmt =
-  kfprintf f cha ("(CastANet) %s: " ^^ fmt ^^ ".\n%!") typ
+  Printf.kfprintf f cha ("(CastANet) %s: " ^^ fmt ^^ ".\n%!") typ
 
 let info fmt = message ignore stdout "Info" fmt 
 let warning fmt = message ignore stderr "Warning" fmt 
