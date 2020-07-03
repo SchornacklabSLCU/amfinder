55bae00a6ea5d355e7b8b710445cdc2b2c80f812
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-07-03 20:32:05 +0100

    Improving build command

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 OUT="castanet_editor.exe"
-
-rm -f "$OUT" 2> /dev/null && \
-dune build "$OUT" && mv "_build/4.08.0/$OUT" . 
+rm -f "$OUT" 2> /dev/null && dune build "$OUT" && mv "_build/4.08.0/$OUT" . 
