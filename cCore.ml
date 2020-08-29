b02e6bc6889f39e2a9eca58ba3ec759379f39fbd
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-08-29 06:00:07 +0100

    Removing annotation types from CCore

--- cCore.ml
+++ cCore.ml
@@ -4,10 +4,6 @@ type icon_size = [ `SMALL | `LARGE ]
 
 type icon_style = [ `GREY | `RGBA ]
 
-type annotation_type = [ `COLONIZATION | `ARB_VESICLES | `ALL_FEATURES ]
-
-let available_annotation_types = [ `COLONIZATION; `ARB_VESICLES; `ALL_FEATURES ]
-
 let edge = 180
 
 let data_dir = "data"
--- cCore.mli
+++ cCore.mli
@@ -8,12 +8,6 @@ type icon_size = [ `SMALL | `LARGE ]
 type icon_style = [ `GREY | `RGBA ]
 (** The type of icon style. *)
 
-type annotation_type = [ `COLONIZATION | `ARB_VESICLES | `ALL_FEATURES ]
-(** The type of annotation styles. *)
-
-val available_annotation_types : annotation_type list
-(** The list of all available annotation types. *)
-
 val edge : int
 
 val data_dir : string
