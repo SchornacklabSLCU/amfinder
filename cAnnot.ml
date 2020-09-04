77e7b8d83af9e86272f552a7fd6003de06579620
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-04 19:27:50 +0100

    Code refactoring and new functions

--- cAnnot.ml
+++ cAnnot.ml
@@ -7,85 +7,86 @@ type hold = string
 
 module type S = sig
   val other : string -> string
-  val colonization : string -> string * string
-  val arb_vesicles : string -> string * string
-  val all_features : string -> string * string
-  val get : CLevel.t -> string -> string * string
+  val get : CLevel.t -> string -> hold * lock
 end
 
-let m_colonization =
+let generator str col arb all =
   let module M = struct
-    let other = EStringSet.diff "YNX"
-    let arb_vesicles = function
-      | "Y" -> "", "NX"
-      | "N" -> "N", "AVX"
-      |  _  -> "X", "AVN"
-    let all_features = function
-      | "Y" -> "", "X"
-      | "N" -> "R", "AVIEHX"
-      |  _  -> "X", "AVIEHR"
-    let colonization = function
-      | "Y" -> "", "NX"
-      | "N" -> "", "YX"
-      |  _  -> "", "YN"
+    let other = EStringSet.diff str
     let get = function
-      | `COLONIZATION -> colonization
-      | `ARB_VESICLES -> arb_vesicles
-      | `ALL_FEATURES -> all_features
+      | `COLONIZATION -> col
+      | `ARB_VESICLES -> arb
+      | `ALL_FEATURES -> all
   end in (module M : S)
+
+let m_colonization = 
+  let col = function
+    | "Y" -> "", "NX"
+    | "N" -> "", "YX"
+    |  _  -> "", "YN"
+  and arb = function
+    | "Y" -> "", "NX"
+    | "N" -> "N", "AVX"
+    |  _  -> "X", "AVN"
+  and all = function
+    | "Y" -> "", "X"
+    | "N" -> "R", "AVIEHX"
+    |  _  -> "X", "AVIEHR"
+  in generator "YNX" ~col ~arb ~all
 
 let m_arb_vesicles =
-  let module M = struct
-    let other = EStringSet.diff "AVNX"
-    let colonization = function
-      | "A" | "V" -> "Y", "NX"
-      | "N" -> "N", "YX"
-      |  _  -> "X", "YN"
-    let all_features = function
-      | "A" -> "AR", "X"
-      | "V" -> "VR", "X"
-      | "N" -> "R", "AVIEHX"
-      |  _  -> "X", "AVIEHR"
-    let arb_vesicles = function
-      | "A" | "V" -> "", "NX"
-      | "N" -> "", "AVX"
-      |  _  -> "", "AVN"
-    let get = function
-      | `COLONIZATION -> colonization
-      | `ARB_VESICLES -> arb_vesicles
-      | `ALL_FEATURES -> all_features
-  end in (module M : S)
+  let col = function
+    | "A" | "V" -> "Y", "NX"
+    | "N" -> "N", "YX"
+    |  _  -> "X", "YN"
+  and arb = function
+    | "A" | "V" -> "", "NX"
+    | "N" -> "", "AVX"
+    |  _  -> "", "AVN"
+  and all = function
+    | "A" -> "AR", "X"
+    | "V" -> "VR", "X"
+    | "N" -> "R", "AVIEHX"
+    |  _  -> "X", "AVIEHR"
+  in generator "AVNX" ~col ~arb ~all
 
 let m_all_features =
-  let module M = struct
-    let other = EStringSet.diff "AVIEHRX"
-    let colonization = function
-      | "A" | "V" | "I" | "E" | "H" -> "Y", "NX"
-      | "R" -> "", "X"
-      |  _  -> "X", "YN"
-    let arb_vesicles = function
-      | "A" -> "A", "NX"
-      | "V" -> "V", "NX"
-      | "I" | "E" | "H" -> "", "NX"
-      | "R" -> "", "X"
-      |  _  -> "X", "AVN"
-    let all_features = function
-      | "A" | "V" | "I" | "H" -> "R", "X"
-      | "E" -> "", "X"
-      | "R" -> "", "X"
-      |  _  -> "", "AVIEHR"
-    let get = function
-      | `COLONIZATION -> colonization
-      | `ARB_VESICLES -> arb_vesicles
-      | `ALL_FEATURES -> all_features
-  end in (module M : S)
+  let col = function
+    | "A" | "V" | "I" | "E" | "H" -> "Y", "NX"
+    | "R" -> "", "X"
+    |  _  -> "X", "YN"
+  and arb = function
+    | "A" -> "A", "NX"
+    | "V" -> "V", "NX"
+    | "I" | "E" | "H" -> "", "NX"
+    | "R" -> "", "X"
+    |  _  -> "X", "AVN"
+  and all = function
+    | "A" | "V" | "I" | "H" -> "R", "X"
+    | "E" -> "", "X"
+    | "R" -> "", "X"
+    |  _  -> "", "AVIEHR"
+  in generator "AVIEHRX" ~col ~arb ~all
 
 let get = function
   | `COLONIZATION -> m_colonization
   | `ARB_VESICLES -> m_arb_vesicles
   | `ALL_FEATURES -> m_all_features
 
-let get_rule lvl = let open (val (get lvl) : S) in get
+let chars lvl = let open (val (get lvl) : S) in other ""
+
+let char_list lvl = List.init (String.get) (chars lvl)
+
+(* Not hardcoded, should the list change in the future. *)
+let all_chars =
+  let col = chars `COLONIZATION
+  and arb = chars `ARB_VESICLES
+  and all = chars `ALL_FEATURES in
+  EStringSet.(union (union col arb) all)
+
+let all_chars_list = List.init (String.get) all_chars
+
+let rule lvl = let open (val (get lvl) : S) in get
 
 let others elt lvl =
   let str = String.uppercase_ascii (
--- cAnnot.mli
+++ cAnnot.mli
@@ -5,8 +5,20 @@
 type lock = string
 type hold = string
 
-val get_rule : CLevel.t -> CLevel.t -> char -> hold * lock
-(** [get_rule lvl1 lvl2 chr] returns the characters that are locked or on hold
+val chars : CLevel.t -> string
+(** Returns a string containing available annotations at a given level. *)
+
+val char_list : CLevel.t -> char list
+(** Same as above, but returns a list of characters. *)
+
+val all_chars : string
+(** All available characters, irrespective of the annotation level. *)
+
+val all_chars_list : char list
+(** Same as above, but returns a list of characters. *)
+
+val rule : CLevel.t -> CLevel.t -> char -> hold * lock
+(** [rule lvl1 lvl2 chr] returns the characters that are locked or on hold
   * when character [chr] gets activated between levels [lvl1] and [lvl2]. *)
   
 val others : [ `CHR of char | `STR of string ] -> CLevel.t -> string
