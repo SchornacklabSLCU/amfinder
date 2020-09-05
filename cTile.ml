5b8b8b6c9f17f385f8e232ce9e08ab7688353cbd
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-05 11:14:30 +0100

    CNote renamed as CTile

---
+++ cTile.ml
@@ -0,0 +1,31 @@
+(* CastANet - cTile.ml *)
+
+open Scanf
+open Printf
+
+type t = {
+  mutable user : string;
+  mutable lock : string;
+  mutable hold : string;
+}
+
+type layer = [ `USER | `HOLD | `LOCK ]
+
+let create () = { user = ""; lock = ""; hold = "" }
+
+let to_string t = sprintf "%s %s %s" t.user t.lock t.hold
+
+let of_string s = 
+  let import s = sscanf s "%[A-Z] %[A-Z] %[A-Z]"
+    (fun x y z -> {user = x; lock = y; hold = z})
+  in try import s with _ -> invalid_arg s
+
+let get t = function
+  | `USER -> t.user
+  | `LOCK -> t.lock
+  | `HOLD -> t.hold
+
+let set t = function
+  | `USER -> (fun x -> t.user <- x)
+  | `LOCK -> (fun x -> t.lock <- x)
+  | `HOLD -> (fun x -> t.hold <- x)
