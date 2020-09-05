1896cb086b6862bed6e3ca884cbd91f7d41da46f
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-05 20:50:14 +0100

    Removing old modules

--- cEvent.mli
+++
@@ -1,9 +0,0 @@
-(* CastANet - cEvent.mli *)
-
-(** Event manager. *)
-
-val initialize : unit -> (char * GButton.toggle_button * GtkSignal.id) array
-(** Initialize callback functions for all widgets. *)
-
-val update_annotations : GdkEvent.Key.t -> bool
-(** Updates toggle button status based on the key pressed. *)
