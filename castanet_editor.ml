995dd917123563aed7cc995f82ee723f08e9fda7
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-05 20:47:27 +0100

    Updating image loading

--- castanet_editor.ml
+++ castanet_editor.ml
@@ -2,9 +2,6 @@
 
 let _ =
   Printexc.record_backtrace true;
-  print_endline "castanet-editor version 1.0";
-  try
-    let toggles = CEvent.initialize () in
-    CAction.load_image toggles;
-    GMain.main ()
-  with _ -> Printexc.print_backtrace stderr
+  print_endline "castanet-editor version 2.0";
+  CImage.load ();
+  GMain.main ()
