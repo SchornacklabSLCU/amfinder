9fbc53ced0f86e5c61235f393e87281ad6996d00
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-05 11:02:32 +0100

    Major CTable refactoring

--- cTable.ml
+++ cTable.ml
@@ -4,164 +4,102 @@ open CExt
 open Scanf
 open Printf
 
-let auto_background = ref true
-
 type table = {
+  main : CLevel.t;
   colonization : CNote.t EMatrix.t;
   arb_vesicles : CNote.t EMatrix.t;
   all_features : CNote.t EMatrix.t;
-(*network_pred : float array array array;*)
+  network_pred : (string * float array EMatrix.t) list;
 }
 
-let get tbl = function
+let main_level {main; _} = main
+
+let get_matrix_at_level tbl = function
   | `COLONIZATION -> tbl.colonization
   | `ARB_VESICLES -> tbl.arb_vesicles
   | `ALL_FEATURES -> tbl.all_features
 
-type changelog = {
-  log_user : (CLevel.t * string) list;
-  log_lock : (CLevel.t * string) list;
-  log_hold : (CLevel.t * string) list;
-}
+let iter f tbl lvl = EMatrix.iteri f (get_matrix_at_level tbl lvl)
 
-let ensure_not_empty chg =
-  if chg.log_user = []
-  && chg.log_lock = []
-  && chg.log_hold = [] then None
-  else Some chg
-
-
-module Note = struct
-  let rec mem chr t = function
-    | `USER -> String.contains (CNote.get t `USER) chr
-    | `LOCK -> String.contains (CNote.get t `LOCK) chr
-    | `HOLD -> String.contains (CNote.get t `HOLD) chr
-    | `FULL -> List.exists (mem chr t) [`USER; `LOCK; `HOLD]
-    
-  let add lvl1 lvl2 chr note =
-    let usermode = (lvl1 = lvl2) and extra = String.make 1 chr in
-    let log_user, log_lock, log_hold = 
-      if usermode then begin
-        let old = CNote.get note `USER in
-        CNote.set note `USER (EStringSet.union old extra);
-        let d = EStringSet.diff extra old in
-        if d = "" then [], [], [] else [lvl1, d], [], []
-      end else begin
-        let old_lock = CNote.get note `LOCK 
-        and old_hold = CNote.get note `HOLD in
-        let hold, lock = CAnnot.rule lvl1 lvl2 extra in
-        CNote.set note `LOCK (EStringSet.union old_lock lock);
-        CNote.set note `HOLD (EStringSet.union old_hold hold);
-        let dl = EStringSet.diff lock old_lock
-        and dh = EStringSet.diff hold old_hold in
-        ([], (if dl = "" then [] else [lvl1, dl]), 
-             (if dh = "" then [] else [lvl1, dh]))
-      end
-    in {log_user; log_lock; log_hold}
-    
-  let rem lvl1 lvl2 chr note = 
-    let usermode = (lvl1 = lvl2) and del = String.make 1 chr in
-    let log_user, log_lock, log_hold =
-      if usermode then begin
-        let old = CNote.get note `USER in
-        CNote.set note `USER (EStringSet.diff old del);
-        [lvl1, del], [], []
-      end else begin
-        let old_lock = CNote.get note `LOCK 
-        and old_hold = CNote.get note `HOLD in
-        let hold, lock = CAnnot.rule lvl1 lvl2 del in
-        CNote.set note `LOCK (EStringSet.diff old_lock lock);
-        CNote.set note `HOLD (EStringSet.diff old_hold hold);
-        [], [lvl2, lock], [lvl2, hold]
-      end 
-    in {log_user; log_lock; log_hold}
-end
-
-let to_string tbl lvl = EMatrix.to_string ~cast:CNote.to_string (get tbl lvl)
-  
-let of_string ~col:x ~arb:y ~all:z = 
-  let f = EMatrix.of_string ~cast:CNote.of_string in
-  { colonization = f x; arb_vesicles = f y; all_features = f z }
-
-let statistics tbl lvl =
-  let stats = List.map (fun chr -> chr, ref 0) CLevel.all_chars_list in
-  EMatrix.iter (fun note ->
-    EStringSet.union (CNote.get note `USER) (CNote.get note `HOLD)
-    |> String.iter (fun c -> incr (List.assoc c stats))
-  ) (get tbl lvl);
-  List.map (fun (chr, r) -> chr, !r) stats
-
-module Changelog = struct
-  let add ch1 ch2 = {
-    log_user = ch1.log_user @ ch2.log_user;
-    log_lock = ch1.log_lock @ ch2.log_lock;
-    log_hold = ch1.log_hold @ ch2.log_hold;
-  }
-end
-
-let get table lvl ~r ~c typ =
-  let note = (level table lvl).(r).(c) in
-  CNote.get note typ
-
-(* An annotation can be added if it:
-    - has not already been defined (user).
-    - is not forbidden for consistency (lock).
-    - is not activated for consistency (hold). *)
-let add t lvl1 ~r ~c chr =
-  let mat = level t lvl1 in
-  if Note.mem chr mat.(r).(c) `FULL then None else (
-    let log1 = Note.add lvl1 lvl1 chr mat.(r).(c) in
-    let lvl2, lvl3 = CLevel.others lvl1 in
-    let log2 = Note.add lvl1 lvl2 chr (level t lvl2).(r).(c)
-    and log3 = Note.add lvl1 lvl3 chr (level t lvl3).(r).(c) in
-    ensure_not_empty Changelog.(add (add log1 log2) log3)
-  )
-
-(* An annotation can be removed only if it is part of the <user> field.  *)
-let remove t lvl1 ~r ~c chr =
-  let mat = level t lvl1 in
-  if Note.mem chr mat.(r).(c) `USER then (
-    let log1 = Note.rem lvl1 lvl1 chr mat.(r).(c) in
-    let lvl2, lvl3 = CLevel.others lvl1 in
-    let log2 = Note.rem lvl1 lvl2 chr (level t lvl2).(r).(c)
-    and log3 = Note.rem lvl1 lvl3 chr (level t lvl3).(r).(c) in
-    ensure_not_empty Changelog.(add (add log1 log2) log3)
-  ) else None
-
-(* TODO: exports simplified tables for Python work. *)
-let save t zip =
-  let och = Zip.open_out zip in
-  let dat = to_string t `COLONIZATION in
-  Zip.add_entry dat och "colonization.mldata" ~comment:(Digest.string dat);
-  let dat = to_string t `ARB_VESICLES in
-  Zip.add_entry dat och "arb_vesicles.mldata" ~comment:(Digest.string dat);
-  let dat = to_string t `ALL_FEATURES in
-  Zip.add_entry dat och "all_features.mldata" ~comment:(Digest.string dat);
-  Zip.close_out och
-
-let load src =
-  try
-    assert (Sys.file_exists src);
-    let ich = Zip.open_in src in
-    let xe = Zip.find_entry ich "colonization.mldata"
-    and ye = Zip.find_entry ich "arb_vesicles.mldata"
-    and ze = Zip.find_entry ich "all_features.mldata" in
-    let xd = Zip.read_entry ich xe
-    and yd = Zip.read_entry ich ye
-    and zd = Zip.read_entry ich ze in
-    assert (Digest.string xd = xe.Zip.comment);
-    assert (Digest.string yd = ye.Zip.comment);
-    assert (Digest.string zd = ze.Zip.comment);
-    Zip.close_in ich;
-    Some (of_string ~col:xd ~arb:yd ~all:zd)
-  with Assert_failure _ | Zip.Error _ | Sys_error _ -> None
-
-let create src =
+let create ?(main = `COLONIZATION) src =
   let mat = match src with
     | `DIM (r, c) -> EMatrix.init r c (fun _ _ -> CNote.create ())
     | `MAT matrix -> EMatrix.map (fun _ -> CNote.create ()) matrix in
-  { colonization = mat;
+  { main; colonization = mat;
     arb_vesicles = EMatrix.copy mat;
-    all_features = EMatrix.copy mat }
+    all_features = EMatrix.copy mat;
+    network_pred = [] }
 
-let iter f tbl lvl = EMatrix.iteri f (get tbl lvl)
+let to_string tbl lvl =
+  let elt = get_matrix_at_level tbl lvl in
+  EMatrix.to_string ~cast:CNote.to_string elt
+
+let of_string ~main ~col_table ~arb_table ~all_table = 
+  let f = EMatrix.of_string ~cast:CNote.of_string in
+  { main; colonization = f col_table;
+    arb_vesicles = f arb_table;
+    all_features = f all_table;
+    network_pred = [] }
+
+let load zip =
+  let unsafe_load zip =
+    assert (Sys.file_exists zip);
+    let ich = Zip.open_in zip in
+    let col_entry = Zip.find_entry ich "colonization.mldata"
+    and arb_entry = Zip.find_entry ich "arb_vesicles.mldata"
+    and all_entry = Zip.find_entry ich "all_features.mldata" in
+    let main = if col_entry.Zip.extra = "main" then `COLONIZATION
+          else if arb_entry.Zip.extra = "main" then `ARB_VESICLES
+          else if all_entry.Zip.extra = "main" then `ALL_FEATURES
+          else assert false in
+    let col_table = Zip.read_entry ich col_entry
+    and arb_table = Zip.read_entry ich arb_entry
+    and all_table = Zip.read_entry ich all_entry in
+    assert (Digest.string col_table = col_entry.Zip.comment);
+    assert (Digest.string arb_table = arb_entry.Zip.comment);
+    assert (Digest.string all_table = all_entry.Zip.comment);
+    Zip.close_in ich;
+    Some (of_string ~main ~col_table ~arb_table ~all_table)
+  in try unsafe_load zip with _ -> None
+
+let statistics tbl lvl =
+  let stats = List.map (fun chr -> chr, ref 0) (CAnnot.char_list lvl) in
+  EMatrix.iter (fun t ->
+    let annot = EStringSet.union (CNote.get t `USER) (CNote.get t `HOLD) in
+    String.iter (fun c -> incr (List.assoc c stats)) annot
+  ) (get_matrix_at_level tbl lvl);
+  List.map (fun (chr, r) -> chr, !r) stats
+
+type export_flag = [
+  | `USER_ANNOT_ONLY
+  | `AUTO_BACKGROUND
+  | `MAIN_LEVEL_ONLY
+  | `LEVEL of CLevel.t
+  | `PREDICTION of string
+  | `PRED_THRESHOLD of float
+  | `MIN_STDEV of float
+  | `BEST_PREDICTION
+  | `EXPORT_STATISTICS
+]
+
+let save 
+ ?(export = false) 
+ ?(flags = [`AUTO_BACKGROUND; `MAIN_LEVEL_ONLY]) t zip =
+  let d1 = to_string t `COLONIZATION
+  and d2 = to_string t `ARB_VESICLES
+  and d3 = to_string t `ALL_FEATURES in
+  let c1 = Digest.string d1
+  and c2 = Digest.string d2
+  and c3 = Digest.string d3 in
+  let e1, e2, e3 = match main_level t with
+    | `COLONIZATION -> "main", "", ""
+    | `ARB_VESICLES -> "", "main", ""
+    | `ALL_FEATURES -> "", "", "main" in
+  let och = Zip.open_out zip in
+  Zip.add_entry d1 och "colonization.mldata" ~comment:c1 ~extra:e1;
+  Zip.add_entry d2 och "arb_vesicles.mldata" ~comment:c2 ~extra:e2;
+  Zip.add_entry d3 och "all_features.mldata" ~comment:c3 ~extra:e3;
+  (* TODO: insert export here. *)
+  Zip.close_out och
+
+
--- cTable.mli
+++ cTable.mli
@@ -4,47 +4,68 @@ open CExt
 
 (** Multi-level annotation tables. *)
 
-val auto_background : bool ref
-(** If set to [true], tiles lacking annotations are automatically considered
-  * background (default value: [true]). While convenient, this may be disabled
-  * to allow for training using partially annotated pictures. *)
-
 type table
 (** The type for annotation table. Annotation table consists of three matrices
   * corresponding to the different annotation levels (basic, intermediate and
   * complete). *)
-  
-type changelog = {
-  log_user : (CLevel.t * string) list;   (** User-defined annotations. *)
-  log_lock : (CLevel.t * string) list;   (** Switched off annotations. *)
-  log_hold : (CLevel.t * string) list;   (** Switched on annotations.  *)
-}
-(** Changelog indicating the modifications made to the annotations. *)
 
-val add : table -> CLevel.t -> r:int -> c:int -> char -> changelog option
+
+(** {2 Creation and loading} *)
+
+val create :
+  ?main:CLevel.t -> 
+  [ `DIM of (int * int) | `MAT of 'a EMatrix.t ] -> table
+(** Creates an empty matrix with either the given dimensions, or by mapping an
+  * existing matrix. The optional parameter [main] allows to define the main
+  * annotation layer (defaults to [`COLONIZATION]). *)
+
+val load : string -> table option
+(** Imports tables from a ZIP archive. Returns [None] in case of error. *)
+
+
+(** {2 Saving and export} *)
+
+type export_flag = [
+  | `USER_ANNOT_ONLY            (** Exports user-defined annotations only. *)
+  | `AUTO_BACKGROUND            (** Sets unannotated tiles as background.  *)
+  | `MAIN_LEVEL_ONLY            (** Exports the main level only.           *)
+  | `LEVEL of CLevel.t          (** Exports a given level.                 *)
+  | `PREDICTION of string       (** Exports a given CNN prediction.        *)
+  | `PRED_THRESHOLD of float    (** Prediction threshold.                  *)
+  | `MIN_STDEV of float         (** Minimum dispersion value.              *)
+  | `BEST_PREDICTION            (** Keep the best predicted class only.    *)
+  | `EXPORT_STATISTICS          (** Exports statistics (for analysis).     *)
+]
+(** Table export options. These options can be combined. *)
+
+val save : ?export:bool -> ?flags:export_flag list -> table -> string -> unit
+(** Saves the given table as ZIP archive. If the optional parameter [export]
+  * is set to [true], tables are produced for use with the CastANet Python
+  * tools. Use [flags] to control how those tables are generated. *)
+
+
+(** {2 Edition} *)
+
+(*val add : table -> CLevel.t -> r:int -> c:int -> char -> CChangeLog.t option
 (** [add t x ~r ~c chr] adds annotation [chr] at row [r] and column [c] in
   * level [x]-matrix of table [t], and returns a changelog of altered 
   * annotations in all other layers. These changes are to be reflected in the
   * user interface. The function returns [None] if no change was made. *)
 
-val remove : table -> CLevel.t -> r:int -> c:int -> char -> changelog option
+val remove : table -> CLevel.t -> r:int -> c:int -> char -> CChangeLog.t option
 (** Same arguments as [add], but this function tries and remove a given 
   * annotation from a given tile. Again the changelog indicates the triggered 
   * modifications. *)
 
 val get : table -> CLevel.t -> r:int -> c:int -> CNote.layer -> string
 (** [get t lvl ~r ~c nt] returns the annotations of type [nt] at row [r] and
-  * column [c] in layer [lvl] of table [t]. *)
+  * column [c] in layer [lvl] of table [t]. *)*)
 
-val load : string -> table option
-(** Imports tables from a ZIP archive. Returns [None] in case of error. *)
 
-val create : [ `DIM of (int * int) | `MAT of 'a EMatrix.t ] -> table
-(** Creates an empty matrix with either the given dimensions, or by mapping an
-  * existing matrix. *)
+(** {2 Misc operations} *)
 
-val save : table -> string -> unit
-(** [export t s] exports table [t] as ZIP archive [s]. *)
+val main_level : table -> CLevel.t
+(** Returns the main level of the given table. *)
 
 val statistics : table -> CLevel.t -> (char * int) list
 (** [statistics t lvl] returns the counts for each structure at level [lvl]
@@ -52,6 +73,3 @@ val statistics : table -> CLevel.t -> (char * int) list
 
 val iter : (r:int -> c:int -> CNote.t -> unit) -> table -> CLevel.t -> unit
 (** Table iterator. *)
-
-
-
