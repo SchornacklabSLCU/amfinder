3e828d1ba0b9e21e21ba6588c54bb7b4200184f2
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-05 20:48:14 +0100

    First version of the new CImage module

--- cImage.ml
+++ cImage.ml
@@ -1,10 +1,41 @@
 (* CastANet - cImage.mli *)
 
-type tiles = GdkPixbuf.pixbuf CExt.Matrix.t
+open CExt
+open CGUI
+open Printf
 
-type mosaic = { edge : int; matrix : tiles } 
-type sizes = { small : mosaic; large : mosaic }
-type param = { path : string; rows : int; cols : int }
+module ImageOps = struct
+  let crop ~src_x ~src_y ~edge:e pix =
+    let dest = GdkPixbuf.create ~width:e ~height:e () in
+    GdkPixbuf.copy_area ~dest ~src_x ~src_y pix;
+    dest
+      
+  let resize ?(interp = `NEAREST) ~edge:e pix =
+    let open GdkPixbuf in
+    let scale_x = float e /. (float (get_width pix))
+    and scale_y = float e /. (float (get_height pix)) in
+    let dest = create ~width:e ~height:e () in
+    scale ~dest ~scale_x ~scale_y ~interp pix;
+    dest
+end
+
+type tiles = GdkPixbuf.pixbuf EMatrix.t
+
+type mosaic = {
+  edge : int;
+  matrix : tiles;
+}
+ 
+type sizes = {
+  small : mosaic;
+  large : mosaic;
+}
+
+type param = {
+  path : string; 
+  rows : int; 
+  cols : int;
+}
 
 type graph = {
   xini : int;
@@ -21,118 +52,158 @@ type t = {
   graph : graph;
 }
 
-(* Getters *)
-let path t = t.param.path
-let basename t = Filename.basename (path t)
-let dirname t = Filename.dirname (path t)
-
-let source t = function
-  | `W -> t.graph.imgw 
-  | `H -> t.graph.imgh
-
-let origin t = function
-  | `X -> t.graph.xini
-  | `Y -> t.graph.yini
-
-let dim t = function
-  | `R -> t.param.rows
-  | `C -> t.param.cols
-
-let annotations {table; _} = table
-
-let edge t = function
-  | `SMALL -> t.sizes.small.edge 
-  | `LARGE -> t.sizes.large.edge
-
-let tiles t = function
-  | `SMALL -> t.sizes.small.matrix
-  | `LARGE -> t.sizes.large.matrix
-
+(* garbage? 
 let cursor_pos t = t.graph.cursor
 let set_cursor_pos t pos = t.graph.cursor <- pos
+let tile ~r ~c t typ = EMatrix.get_opt (tiles t typ) r c
+let annotation ~r ~c t = EMatrix.get_opt (annotations t) r c
+let is_valid ~r ~c t = EMatrix.get_opt (Mosaic.annotations t) r c <> None
+ /end of garbage *)
 
-let tile ~r ~c t typ = CExt.Matrix.get_opt (tiles t typ) r c
-let annotation ~r ~c t = CExt.Matrix.get_opt (annotations t) r c
+(* Image currently being processed. *)
+let active_image = ref None
 
-let iter_tiles f t typ = CExt.Matrix.iteri f (tiles t typ)
-
-let x ~c t typ = (origin t `X) + c * (edge t typ)
-let y ~r t typ = (origin t `Y) + r * (edge t typ)
-
-let is_valid ~r ~c t = CExt.Matrix.get_opt (annotations t) r c <> None
-
-
-module Binary = struct
-  let binfile path = Filename.remove_extension path ^ ".bin"
-  (* Save image as binary at exit. *)
-  let save_at_exit img =
-    let bin = binfile (path img) in
-    (* Faster than erasing file contents. *)
-    if Sys.file_exists bin then Sys.remove bin;
-    let och = open_out_bin bin in
-    output_value och img;
-    close_out och
-  (* Restore image at startup from binary data. *)
-  let restore path =
-    let bin = binfile path in
-    if Sys.file_exists bin then (
-      CLog.info "Loading binary file '%s'" (Filename.basename bin);
-      let ich = open_in_bin bin in
-      let img = input_value ich in
-      close_in ich;
-      Some img
-    ) else None
+module Par = struct
+  open Arg
+  let edge = ref 236
+  let image_path = ref None
+  let set_image_path x = if Sys.file_exists x then image_path := Some x
+  let usage = "castanet_editor.exe [OPTIONS] [IMAGE_PATH]"
+  let specs = align [
+    "--edge", Set_int edge, sprintf " Tile size (default: %d pixels)." !edge;
+  ]
+  let initialize () = parse specs set_image_path usage
 end
+
+module Info = struct
+  let path t = t.param.path
+  let basename t = Filename.basename (path t)
+  let dirname t = Filename.dirname (path t)
+end
+
+module Mosaic = struct
+  let source t = function
+    | `W -> t.graph.imgw 
+    | `H -> t.graph.imgh
+
+  let origin t = function
+    | `X -> t.graph.xini
+    | `Y -> t.graph.yini
+
+  let dim t = function
+    | `R -> t.param.rows
+    | `C -> t.param.cols
+
+  let edge t = function
+    | `SMALL -> t.sizes.small.edge 
+    | `LARGE -> t.sizes.large.edge
+
+  let tiles t = function
+    | `SMALL -> t.sizes.small.matrix
+    | `LARGE -> t.sizes.large.matrix
+
+  let annotations {table; _} = table
+  let x ~c t typ = (origin t `X) + c * (edge t typ)
+  let y ~r t typ = (origin t `Y) + r * (edge t typ)
+end
+
+
+module Iter = struct
+  let tiles f t typ = EMatrix.iteri f (Mosaic.tiles t typ)
+end
+
+
+
+
 
 
 module Create = struct
-  open CExt
   let large_tile_matrix nr nc edge src =
-    Matrix.init nr nc (fun r c ->
+    EMatrix.init nr nc (fun r c ->
       let src_x = c * edge and src_y = r * edge in
-      Image.(crop_square ~src_x ~src_y ~edge src |> resize ~edge:180)
+      ImageOps.crop ~src_x ~src_y ~edge src |> ImageOps.resize ~edge:180
     )
     
-  let small_tile_matrix edge = Matrix.map (Image.resize ~edge)
+  let small_tile_matrix edge = EMatrix.map (ImageOps.resize ~edge)
     
   let annotations path tiles =
     let zip = Filename.remove_extension path ^ ".zip" in
-    if Sys.file_exists zip then CTable.load zip
-    else CTable.create (`MAT tiles)
+    if Sys.file_exists zip then CTable.load zip |> Option.get (* FIXME *)
+    else CTable.create (`MAT tiles) 
 end
 
 let create ~ui_width:uiw ~ui_height:uih path =
-  let img = match Binary.restore path with
-    | Some img -> img
-    | None -> let pix = GdkPixbuf.from_file path in
-      let imgw, imgh = GdkPixbuf.(get_width pix, get_height pix) in
-      let edge = CSettings.edge () in
-      let rows = imgh / edge and cols = imgw / edge in
-      let large = Create.large_tile_matrix rows cols edge pix in
-      let sub = min (uiw / cols) (uih / rows) in
-      let small = Create.small_tile_matrix sub large in
-      let table = Create.annotations path small in
-      let graph = {
-        imgw; imgh; cursor = (0, 0);
-        xini = (uiw - sub * cols) / 2;
-        yini = (uih - sub * rows) / 2;
-      } and sizes = {
-        small = {edge = sub; matrix = small};
-        large = {edge = 180; matrix = large};
-      } and param = {path; rows; cols} in
-      let img = { param; sizes; graph; table } in
-      CPalette.set_tile_edge sub;
-      CLog.info "source image: '%s'" path;
-      CLog.info "source image size: %d x %d pixels" imgw imgh;
-      CLog.info "tile matrix: %d x %d; edge: %d pixels" rows cols edge;
-      img
-  in img
+  let pix = GdkPixbuf.from_file path in
+  let imgw, imgh = GdkPixbuf.(get_width pix, get_height pix) in
+  let edge = !Par.edge in
+  let rows = imgh / edge and cols = imgw / edge in
+  let large = Create.large_tile_matrix rows cols edge pix in
+  let sub = min (uiw / cols) (uih / rows) in
+  let small = Create.small_tile_matrix sub large in
+  let table = Create.annotations path small in
+  let graph = {
+    imgw; imgh; cursor = (0, 0);
+    xini = (uiw - sub * cols) / 2;
+    yini = (uih - sub * rows) / 2;
+  } and sizes = {
+    small = {edge = sub; matrix = small};
+    large = {edge = 180; matrix = large};
+  } and param = {path; rows; cols} in
+  let img = { param; sizes; graph; table } in
+  CPalette.set_tile_edge sub;
+  CLog.info "source image: '%s'" path;
+  CLog.info "source image size: %d x %d pixels" imgw imgh;
+  CLog.info "tile matrix: %d x %d; edge: %d pixels" rows cols edge;
+  img
 
-let statistics img = CTable.statistics (annotations img)
+let statistics img = CTable.statistics (Mosaic.annotations img)
    
 let digest t =
-  Printf.sprintf "<small><tt> \
+  sprintf "<small><tt> \
     <b>Image:</b> %s ▪ \
     <b>Size:</b> %d × %d pixels ▪ \
     <b>Tiles:</b> %d × %d</tt></small>" 
-  (basename t) (source t `W) (source t `H) (dim t `R) (dim t `C)
+    (Info.basename t)
+    (Mosaic.source t `W) (Mosaic.source t `H)
+    (Mosaic.dim t `R) (Mosaic.dim t `C)
+
+
+module Paint = struct
+  let white_background () =
+    let t = GUI_Drawing.cairo () in
+    Cairo.set_source_rgba t 1.0 1.0 1.0 1.0;
+    let w = float (GUI_Drawing.width ()) 
+    and h = float (GUI_Drawing.height ()) in
+    Cairo.rectangle t 0.0 0.0 ~w ~h;
+    Cairo.fill t;
+    Cairo.stroke t;
+    GUI_Drawing.synchronize ()
+
+  let tiles t =
+    let pixmap = GUI_Drawing.pixmap ()
+    and xini = Mosaic.origin t `X
+    and yini = Mosaic.origin t `Y
+    and edge = Mosaic.edge t `SMALL in
+    Iter.tiles (fun ~r ~c tile ->
+      pixmap#put_pixbuf
+        ~x:(xini + c * edge)
+        ~y:(yini + r * edge)
+        ~width:edge ~height:edge tile
+    ) t `SMALL;
+    GUI_Drawing.synchronize ()
+end
+
+
+let load () =
+  Par.initialize ();
+  let path = match !Par.image_path with
+    | None -> GUI_FileChooserDialog.run ()
+    | Some path -> path in
+  CGUI.window#show ();
+  let ui_width = GUI_Drawing.width ()
+  and ui_height = GUI_Drawing.height () in
+  let t = create ~ui_width ~ui_height path in
+  active_image := Some t;
+  Paint.white_background ();
+  Paint.tiles t;
+  CGUI.status#set_label (digest t)
