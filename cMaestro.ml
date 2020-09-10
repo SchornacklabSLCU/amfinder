8c7c6663edf39ee9ecb2f13b7d4d15f0fbe8b913
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-10 22:33:29 +0100

    Updating CPaint functions

--- cMaestro.ml
+++ cMaestro.ml
@@ -18,155 +18,6 @@ module Par = struct
 end
 
 
-
-
-(* Cairo surfaces for the painting functions below. *)
-module Img_Surface = struct
-  let square ?(alpha = 0.85) ~kind edge =
-    assert (edge > 0); 
-    let surface = Cairo.Image.(create ARGB32 ~w:edge ~h:edge) in
-    let t = Cairo.create surface in
-    Cairo.set_antialias t Cairo.ANTIALIAS_SUBPIXEL;
-    let clr = match kind with 
-      | `CURSOR -> "#cc0000" 
-      | `RGB x -> x in
-    let r, g, b = EColor.html_to_float clr
-    and a = max (min alpha 1.0) 0.0 in
-    Cairo.set_source_rgba t r g b a;
-    let edge = float edge in
-    Cairo.rectangle t 0.0 0.0 ~w:edge ~h:edge;
-    Cairo.fill t;
-    Cairo.stroke t;
-    surface
-
-  let joker = 
-    let aux () =
-      match (CImage.get_active ()) with
-      | None -> assert false (* does not happen. *)
-      | Some img -> let edge = CImage.edge img `SMALL in
-        square ~kind:(`RGB "#aaffaa") edge
-    in Ext_Memoize.create ~label:"Img_Surface.master" aux
-
-  let cursor = 
-    let aux () =
-      match (CImage.get_active ()) with
-      | None -> assert false (* does not happen. *)
-      | Some img -> let edge = CImage.edge img `SMALL in
-        square ~kind:`CURSOR edge
-    in Ext_Memoize.create ~label:"Img_Surface.cursor" aux
-
-  let pointer = 
-    let create () = 
-      match (CImage.get_active ()) with
-      | None -> assert false
-      | Some img -> let edge = CImage.edge img `SMALL in
-        square ~kind:`CURSOR ~alpha:0.40 edge
-    in Ext_Memoize.create ~label:"Img_Surface.pointer" create
-
-  let layers =
-    List.map (fun lvl ->
-      let aux lvl () =
-        match (CImage.get_active ()) with
-        | None -> assert false (* does not happen. *)
-        | Some img -> let edge = CImage.edge img `SMALL in   
-          List.map2 (fun chr rgb ->
-            chr, square ~kind:(`RGB rgb) edge
-          ) (CAnnot.char_list lvl) (CLevel.colors lvl)
-      in lvl, Ext_Memoize.create ~label:"Img_Surface.layers" (aux lvl)
-    ) CLevel.flags
-
-  let get = function
-    | '*' -> joker ()
-    | '.' -> cursor ()
-    | chr -> let lvl = GUI_levels.current () in
-      List.assoc chr (List.assoc lvl layers ()) 
-end
-
-
-(* Painting functions. *)
-module Img_Paint = struct
-  let white_background ?(sync = true) () =
-    let t = GUI_Drawing.cairo () in
-    Cairo.set_source_rgba t 1.0 1.0 1.0 1.0;
-    let w = float (GUI_Drawing.width ()) 
-    and h = float (GUI_Drawing.height ()) in
-    Cairo.rectangle t 0.0 0.0 ~w ~h;
-    Cairo.fill t;
-    Cairo.stroke t;
-    if sync then GUI_Drawing.synchronize ()
-
-  let tiles ?(sync = true) () =
-    Option.iter (fun img ->
-      let pixmap = GUI_Drawing.pixmap ()
-      and xini = CImage.origin img `X
-      and yini = CImage.origin img `Y
-      and edge = CImage.edge img `SMALL in
-      CImage.iter_tiles (fun ~r ~c tile ->
-        pixmap#put_pixbuf
-          ~x:(xini + c * edge)
-          ~y:(yini + r * edge)
-          ~width:edge ~height:edge tile
-      ) img `SMALL;
-      if sync then GUI_Drawing.synchronize ()
-    ) (CImage.get_active ())
-
-  let tile ?(sync = false) r c =
-    Option.iter (fun img ->
-      Option.iter (fun tile ->
-        (GUI_Drawing.pixmap ())#put_pixbuf
-          ~x:(CImage.x ~c img `SMALL)
-          ~y:(CImage.y ~r img `SMALL) tile;
-        if sync then GUI_Drawing.synchronize ()
-      ) (CImage.tile r c img `SMALL)
-    ) (CImage.get_active ())
-
-  (* FIXME Unsafe function - not for use outside! *)
-  let surface ?(sync = false) r c surface =
-    Option.iter (fun img ->
-      let t = GUI_Drawing.cairo () in
-      let x = CImage.x ~c img `SMALL 
-      and y = CImage.y ~r img `SMALL in
-      Cairo.set_source_surface t surface (float x) (float y);
-      Cairo.paint t;
-      if sync then GUI_Drawing.synchronize ()
-    ) (CImage.get_active ())
-
-  let annot ?(sync = false) r c =
-    Option.iter (fun img ->
-      let typ = GUI_Layers.get_active ()
-      and tbl = CImage.annotations img
-      and lvl = GUI_levels.current () in
-      let draw = match typ with
-        | '*' -> not (CTable.is_empty tbl lvl ~r ~c) (* Catches any annotation. *)
-        | chr -> CTable.mem tbl lvl r c (`CHR chr) in
-      if draw then begin
-        surface r c (Img_Surface.get typ);
-        if sync then GUI_Drawing.synchronize ()
-      end
-    ) (CImage.get_active ())
-
-  let cursor ?(sync = false) () =
-    Option.iter (fun img ->
-      let r, c = CImage.cursor_pos img in
-      tile r c;
-      surface r c (Img_Surface.get '.');
-      if sync then GUI_Drawing.synchronize ()
-    ) (CImage.get_active ())
-
-  let active_layer ?(sync = true) () =
-    Option.iter (fun img ->
-      CTable.iter (fun ~r ~c _ ->
-        tile r c;
-        annot r c
-      ) (CImage.annotations img) (GUI_levels.current ());
-      cursor ();
- (*     let r, c = CImage.cursor_pos img in
-      Img_UI_update.set_coordinates r c; *)
-      if sync then GUI_Drawing.synchronize ()
-    ) (CImage.get_active ())
-end
-
-
 (* Interaction with the user interface. *)
 module Img_UI_update = struct
   let set_coordinates =
@@ -222,15 +73,15 @@ module Img_Move = struct
     Option.iter (fun img ->
       (* Cursor gets removed; we need to repaint the tile. *)
       let r, c = CImage.cursor_pos img in
-      Img_Paint.tile r c;
-      Img_Paint.annot r c;
+      CPaint.tile ~r ~c ();
+      CPaint.annot ~r ~c ();
       (* Moves to the new cursor position. *)
       let new_r, new_c = f_row r, f_col c in
       CImage.set_cursor_pos img (new_r, new_c);
       Img_UI_update.set_coordinates new_r new_c;
       Img_UI_update.magnified_view ();
       Img_UI_update.update_toggles ();
-      Img_Paint.cursor ();
+      CPaint.cursor ();
       GUI_Drawing.synchronize ()
     ) (CImage.get_active ())
 
@@ -302,9 +153,9 @@ let load () =
   (* Loads the image, creates tiles and populates the main window. *)
   let t = CImage.create ~edge:!Par.edge path in
   (* Draws background and tiles, then adds image info to the status bar. *)
-  Img_Paint.white_background ~sync:false ();
-  Img_Paint.tiles ();
-  Img_Paint.active_layer ();
+  CPaint.white_background ~sync:false ();
+  CPaint.tiles ();
+  CPaint.active_layer ();
   CGUI.status#set_label (digest t);
   at_exit save (* FIXME this may not be the ideal situation! *)
 
@@ -314,9 +165,9 @@ module Img_Tracker = struct
 
   let erase ?(sync = false) img =
     Gaux.may (fun ((r, c) as pos) ->
-      Img_Paint.tile r c;
-      Img_Paint.annot r c;
-      if pos = CImage.cursor_pos img then Img_Paint.cursor ();
+      CPaint.tile ~r ~c ();
+      CPaint.annot ~r ~c ();
+      if pos = CImage.cursor_pos img then CPaint.cursor ();
       if sync then GUI_Drawing.synchronize ()
     ) !mem
 
@@ -324,8 +175,8 @@ module Img_Tracker = struct
     erase img;
     if CTable.is_valid (CImage.annotations img) ~r ~c then begin
       erase img;
-      Img_Paint.tile r c;
-      Img_Paint.surface r c (Img_Surface.pointer ());
+      CPaint.tile ~r ~c ();
+      CPaint.surface ~r ~c (CPaint.Surface.pointer ());
       mem := Some (r, c)
     end;
     GUI_Drawing.synchronize ()
@@ -408,9 +259,9 @@ let initialize () =
   ignore (connect#leave_notify Img_Trigger.mouse_leave);
   (* Repaints the tiles when the active layer changes. *)
   GUI_Layers.set_callback (fun _ r _ _ -> 
-    if r#get_active then Img_Paint.active_layer ());
+    if r#get_active then CPaint.active_layer ());
   (* Repaints the tiles when the annotation level changes. *)
   GUI_levels.set_callback (fun _ r ->
-    if r#active then Img_Paint.active_layer ())
+    if r#active then CPaint.active_layer ())
 
 
