d2d77da0652f7a7775bc14abeefae4f31a173a8a
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-11 06:15:12 +0100

    Use dot -x

--- doc/graph/run.sh
+++ doc/graph/run.sh
@@ -6,4 +6,4 @@ ocamldep -one-line *.ml *.mli \
   | grep -v "cmx" \
   | ./doc/graph/mkgraph.py \
   | tee ./doc/graph/dep_graph.dot \
-  | dot -Tpng > ./doc/graph/dep_graph.png
+  | dot -x -Tpng > ./doc/graph/dep_graph.png
