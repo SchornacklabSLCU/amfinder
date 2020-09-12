660bf1037579459689c57dfbb68a428706be3b7b
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-12 06:30:59 +0100

    Adapt script to new source path

--- doc/graph/run.sh
+++ doc/graph/run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-cd ../..
+cd ../../sources
 
 ocamldep -one-line *.ml *.mli \
   | grep -v "cmx" \
-  | ./doc/graph/mkgraph.py \
-  | tee ./doc/graph/dep_graph.dot \
-  | dot -x -Tpng > ./doc/graph/dep_graph.png
+  | ../doc/graph/mkgraph.py \
+  | tee ../doc/graph/dep_graph.dot \
+  | dot -x -Tpng > ../doc/graph/dep_graph.png
