a3f6a69d7f5a69890396b6afde3dbddafde7a897
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-12 21:57:13 +0100

    Improving handling of color palettes

--- sources/uI_Palette.ml
+++ sources/uI_Palette.ml
@@ -1,5 +1,7 @@
 (* uI_Palette.ml *)
 
+open CExt
+open Scanf
 open Printf
 
 module type PARAMS = sig
@@ -7,16 +9,49 @@ module type PARAMS = sig
 end
 
 module type S = sig
+  val ids : unit -> string list
+  val colors : id:string -> string array option
+  val current : unit -> string
+  val set_current : id:string -> unit
   val toolbar : GButton.toolbar
-  val current : unit -> CIcon.palette
-  val viridis : GButton.radio_tool_button * GMisc.image
-  val cividis : GButton.radio_tool_button * GMisc.image
-  val plasma : GButton.radio_tool_button * GMisc.image
+  val palette : GButton.tool_button
 end
 
+type palette = string array
+
+let folder = "data/palettes"
+let palette_db = ref []
+
+let validate_color ?(default = "#ffffffff") s =
+  ksscanf s (fun _ _ -> default) "#%02x%02x%02x" 
+    (sprintf "#%02x%02x%02xcc")
+
+let load () =
+  Array.fold_left (fun pal elt ->
+    let path = Filename.concat folder elt in
+    if Filename.check_suffix path ".palette" then (
+      let base = Filename.remove_extension elt in
+        let colors = Ext_File.read path
+          |> String.split_on_char '\n'
+          |> List.map validate_color
+          |> Array.of_list
+        in (base, colors) :: pal
+    ) else pal
+  ) [] (Sys.readdir folder)
+
+
 module Make (P : PARAMS) : S = struct
-  let current_palette = ref `VIRIDIS
-  let current () = !current_palette
+
+  let palette_db = ref (load ())
+  let curr = ref (fst (List.hd !palette_db))
+  
+  let ids () = fst (List.split !palette_db)
+  let colors ~id = List.assoc_opt id !palette_db
+
+  let current () = !curr
+  let set_current ~id =
+    if List.mem_assoc id !palette_db then curr := id
+    else CLog.warning "%s is not a valid palette identifier" id
 
   let toolbar = GButton.toolbar
     ~orientation:`VERTICAL
@@ -26,28 +61,19 @@ module Make (P : PARAMS) : S = struct
 
   let packing = toolbar#insert
 
-  let _ = UI_Helper.separator packing; UI_Helper.label packing "Predictions"
+  let _ =
+    UI_Helper.separator packing;
+    UI_Helper.label packing "Predictions"
 
-  let radio_tool_button ?(active = false) ?group ~label typ =
-    let radio = GButton.radio_tool_button ~active ?group ~packing () in
-    let hbox = GPack.hbox ~packing:radio#set_icon_widget () in
+   
+
+  let palette =
+    let button = GButton.tool_button ~packing () in
+    let hbox = GPack.hbox ~packing:button#set_icon_widget () in
     let image = GMisc.image ~width:20 ~packing:(hbox#pack ~expand:false) () in
-    ignore (GMisc.label ~markup:(sprintf "<span size='x-small'>%s</span>" label)
+    image#set_pixbuf (CIcon.get_palette `PLASMA `SMALL);
+    ignore (GMisc.label 
+      ~markup:"<span size='x-small'>Palette</span>"
       ~packing:(hbox#pack ~fill:true) ());
-    image#set_pixbuf (CIcon.get_palette typ `SMALL);
-    radio, image
-
-  let viridis = radio_tool_button ~active:true ~label:"Viridis" `VIRIDIS
-
-  let group = fst viridis
-
-  let cividis = radio_tool_button ~group  ~label:"Cividis" `CIVIDIS
-
-  let plasma = radio_tool_button ~group  ~label:"Plasma" `PLASMA
-
-  let labelled_button label =
-    let btn = GButton.tool_button ~packing () in
-    ignore (GMisc.label ~markup:(sprintf "<small>%s</small>" label)
-      ~packing:btn#set_icon_widget ());
-    btn
+    button
 end
--- sources/uI_Palette.mli
+++ sources/uI_Palette.mli
@@ -1,24 +1,29 @@
 (* CastANet - uI_Palette.mli *)
 
+(** GUI auxiliary module. Loads color palettes. *)
+
 module type PARAMS = sig
   val packing : GObj.widget -> unit
 end
 
 module type S = sig
+  val ids : unit -> string list
+  (** Returns the list of available palette identifiers. *)
+  
+  val colors : id:string -> string array option
+  (** Returns the colors associated with the given palette identifier. *)
+  
+  val current : unit -> string
+  (** Returns the current palette identifier. *)
+  
+  val set_current : id:string -> unit
+  (** Sets the current palette identifier. *)
+  
   val toolbar : GButton.toolbar
   (** Main container. *)
 
-  val current : unit -> CIcon.palette
-  (** Returns the current palette. *)
-
-  val viridis : GButton.radio_tool_button * GMisc.image
-  (** Activates the viridis palette. *)
-  
-  val cividis : GButton.radio_tool_button * GMisc.image
-  (** Activates the cividis palette. *)
-  
-  val plasma : GButton.radio_tool_button * GMisc.image
-  (** Activates the plasma palette. *)
+  val palette : GButton.tool_button
+  (** Displays a small utility window to select a palette. *)
 end
 
 module Make : PARAMS -> S
