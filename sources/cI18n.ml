fcfa46258a1eb7421bcb75b7fb06c2b9ac19f1e6
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-16 10:16:23 +0100

    Internationalization

---
+++ sources/cI18n.ml
@@ -0,0 +1,30 @@
+(* CastANet - cI18n.ml *)
+
+type sentence = [
+  | `CURRENT_PALETTE
+
+]
+
+
+module Lang = struct
+  let english = [
+    `CURRENT_PALETTE, "Current palette:"
+  ]
+  let french = [
+    `CURRENT_PALETTE, "Palette actuelle :"
+  ]
+end
+
+let languages = [
+  "english", Lang.english;
+  "french", Lang.french;
+]
+
+let current = ref "english"
+
+let set ~lang:s =
+  let id = String.lowercase_ascii s in
+  if List.mem_assoc id languages then current := id
+  else CLog.warning "Unknown language %s" s
+
+let get elt = List.assoc elt (List.assoc !current languages)
