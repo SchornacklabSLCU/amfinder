2d181b78b63daaded7d455db45a11e03016ddefe
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-18 21:44:44 +0100

    Code refactoring and documentation

--- python-scripts/castanet_model.py
+++ python-scripts/castanet_model.py
@@ -1,11 +1,22 @@
 # CastANet - castanet_model.py
 
-from keras.initializers import he_uniform
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.initializers import he_uniform
+
+import castanet_config as cConfig
+
 
 
 def core_model(input_shape):
+  """ This function builds the core models, i.e. the successive
+      convolutions and maximum pooling, as well as the hidden dense
+      layers. The output layer is left undefined and will be tuned
+      to fit the annotation level (see functions below). """
   model = Sequential()                                 # 62
   model.add(Conv2D(32, kernel_size=3, name='CONV-1',
                    input_shape=input_shape, activation='relu',
@@ -45,10 +56,13 @@ def core_model(input_shape):
   return model
 
 
-# Simple mode (colonization): colonized, non-colonized, background.
-# Categories are mutually exclusive, therefore we use categorical
-# crossentropy and softmax activation.
+
 def colonization(input_shape):
+  """ This function returns a simple model for the less precise level
+      of annotation, i.e. 'colonization', which has three mutually 
+      exclusive categories: colonized, non-colonized, and background.
+      As a result, the final layer uses categorical cross-entropy and
+      softmax activation. """
   model = core_model(input_shape)
   model.add(Dense(3, activation='softmax', name='DENSE-3',
                   kernel_initializer=he_uniform()))
@@ -58,10 +72,13 @@ def colonization(input_shape):
   return model
 
 
-# Intermediate mode, with arbuscules and vesicles, non-colonized, background.
-# Two categories at least can be combined, therefore we use binary
-# crossentropy and sigmoid activation.
+
 def arb_vesicles(input_shape):
+  """ This function returns a slightly more elaborate model for the
+      intermediate level of annotation, i.e. 'arb_vesicles' which has
+      four categories: arbuscules, vesicles, non-colonized roots, and
+      background. As a result, the final layer uses binary
+      cross-entropy and sigmoid activation. """
   model = core_model(input_shape)
   model.add(Dense(4, activation='sigmoid', name='DENSE-3',
                   kernel_initializer=he_uniform()))
@@ -71,7 +88,23 @@ def arb_vesicles(input_shape):
   return model
 
 
-def from_string(id, input_shape):
+
+def get_input_shape(level):
+  """ This function retrieves the input shape corresponding to
+      the desired annotation level. """
+  if level == 'colonization':
+    return (62, 62, 3)
+  elif level == 'arb_vesicles':
+    return (62, 62, 3)
+  else: # level == 'all_features'
+    return (236, 236, 3)
+
+
+def get():
+  """ This function returns a fresh compiled neural network that
+      is ready for training. """
+  level = cConfig.get('level')
+  input_shape = get_input_shape(level) 
   if id == 'colonization':
     return colonization(input_shape)
   elif id == 'arb_vesicles':
@@ -80,5 +113,5 @@ def from_string(id, input_shape):
     print('WARNING: Not implemented yet')
     return None
   else:
-    print('WARNING: Unknown identifier {id}')
+    print('WARNING: Unknown annotation level {}'.format(level))
     return None
