f03c8b61c010158907d292e5a01928a4cf77ebfe
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-19 13:33:45 +0100

    Improved function <make_table>

--- python-scripts/castanet_predict.py
+++ python-scripts/castanet_predict.py
@@ -3,9 +3,11 @@
 import os
 import numpy as np
 import pandas as pd
-import castanet_config as cfg
-import castanet_segmentation as segm
 from keras.preprocessing import image as keras_image
+
+import castanet_config as cConfig
+import castanet_segmentation as cSegm
+
 
 XY = ['row', 'col']
 
@@ -17,7 +19,7 @@ def get_input_rows(image, tile_count_w, tile_count_h):
     this_row_list = []
     this_col_index_list = list(range(tile_count_w))
     for col in range(tile_count_w): # iter columns.
-      curr = segm.tile(image, row, col)
+      curr = cSegm.tile(image, row, col)
       if curr is None:
         this_col_index_list.remove(col)
         continue
@@ -40,20 +42,27 @@ def save(data, img_path):
   data.to_csv(path, sep='\t', encoding='utf-8', index=False)
 
 
-def predict(cnn, img_path, image, row_index, col_lists, tile_lists):
-  data = []
-  for row, cols, tiles in zip(row_index, col_lists, tile_lists):
-    annot = cnn.predict(tiles, batch_size=cfg.get('batch_size'), verbose=0)
-    annot = [x.reshape(x.shape[0]) for x in annot]   
-    annot = np.array(annot)
-    annot = pd.DataFrame(annot)
-    annot.insert(0, column=XY[1], value=cols)
-    annot.insert(0, column=XY[0], value=[row]*len(cols))
-    data.append(annot)
 
-  data = pd.concat(data, ignore_index=True)
-  data.columns = XY + cfg.get('header')
-  save(data, img_path)
+def make_table(cnn, tiles):
+  """ This function creates a prediction table by iterating a list
+      of Numpy arrays containing tiles for each row. """
+  nrows = len(tiles)
+  if nrows == 0:
+    return None
+  else:
+    bs = cConfig.get('batch_size')
+    results = [pd.DataFrame(cnn.predict(t, batch_size=bs)) for t in tiles]
+    # Generates the final table.
+    table = pd.concat(results, ignore_index=True)
+    table.columns = cConfig.get('header')   
+    # Inserts row and column indexes.
+    ncols = len(results[0])
+    col_values = list(range(ncols)) * nrows
+    row_values = [x // ncols for x in range(nrows * ncols)]
+    table.insert(0, column='col', value=col_values)
+    table.insert(0, column='row', value=row_values)
+    return table
+
 
 
 def run(cnn, input_files):
@@ -61,12 +70,15 @@ def run(cnn, input_files):
       name = os.path.basename(img_path)
       print('* Predicting annotations for "{}"'.format(name))
       image = keras_image.load_img(img_path, color_mode='rgb', target_size=None)
-      edge = cfg.get('source_tile_edge')
+      edge = cConfig.get('tile_edge')
       tile_count_w = image.width // edge
       tile_count_h = image.height // edge
       row_index, col_lists, tile_lists = get_input_rows(image, tile_count_w,
                                                         tile_count_h)
-      predict(cnn, img_path, image, row_index, col_lists, tile_lists)
-
+      ncols = len(col_lists[0])
+      print(type(tile_lists[0]))
+      table = make_table(cnn, tile_lists)
+      print(table.head(32))
+      #save(table, img_path)
 
 
