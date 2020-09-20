628b8ffefb6b69312f4ed6f0e2f6cab91d78e71f
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-20 10:54:56 +0100

    Save output table as archive

--- python-scripts/castanet_save.py
+++ python-scripts/castanet_save.py
@@ -1,10 +1,53 @@
 # CastANet - castanet_save.py
 
 import os
+import sys
+import datetime
+import zipfile as zf
+
+import castanet_config as cConfig
+
+CORRUPTED_ARCHIVE = 30
+
+
+def now():
+    """ Returns the current date/time in a format 
+        suitable for use as file name. """
+
+    return datetime.datetime.now().isoformat(sep='_')
+
 
 
 def archive(results, path):
-  if results is not None:
-    base = os.path.splitext(path)[0]
-    path = '{}.autotags.tsv'.format(base)
-    results.to_csv(path, sep='\t', encoding='utf-8', index=False)
+    """ Saves or append predictions to an archive. """
+
+    if results is not None:
+
+        data = results.to_csv(sep='\t', encoding='utf-8', index=False)
+        zipf = '{}.zip'.format(os.path.splitext(path)[0])
+        tsv = os.path.join('predictions', now() + '.tsv')
+
+        if os.path.isfile(zipf):
+
+            if zf.is_zipfile(zipf):
+
+                print('    -> saved as {}'.format(zipf))
+                with zf.ZipFile(zipf, 'a') as z:
+                    z.comment = cConfig.get('level').encode('utf-8')
+                    z.writestr(tsv, data)
+
+            else:
+
+                print(f'ERROR: Corrupted archive {zipf}')
+                sys.exit(CORRUPTED_ARCHIVE)
+    
+        else:
+
+            print('    -> saved as {}'.format(zipf))
+            with zf.ZipFile(zipf, 'w') as z:
+                z.comment = cConfig.get('level').encode('utf-8')
+                z.writestr(tsv, data)
+
+    else:
+
+        print("WARNING: Empty table")
