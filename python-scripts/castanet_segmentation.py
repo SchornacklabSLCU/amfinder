84c969696cc1d2c08432dc0de5cd5f2ac3670c02
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-20 19:19:48 +0100

    cSegm.tile now uses pyvips

--- python-scripts/castanet_segmentation.py
+++ python-scripts/castanet_segmentation.py
@@ -2,6 +2,7 @@
 
 import random
 import pyvips
+import functools
 import numpy as np
 
 import castanet_config as cConfig
@@ -15,40 +16,11 @@ def tile(image, r, c):
         model input size, and returns it as a NumPy array. """
 
     edge = cConfig.get('tile_edge')
+
     tile = image.crop(c * edge, r * edge, edge, edge)
-    scale = cConfig.get('model_input_size') / edge
-    tile = tile.resize(scale, interpolate=INTERP_NEAREST)
+    tile = tile.resize(cConfig.get('model_input_size') / edge,
+                       interpolate=INTERP_NEAREST)
 
-    buffer = tile.write_to_memory()
-    return np.ndarray(buffer=buffer, dtype=np.uint8,
+    return np.ndarray(buffer=tile.write_to_memory(),
+                      dtype=np.uint8,
                       shape=[tile.height, tile.width, tile.bands])
-
-
-
-def drop_background(tiles, hot_labels):
-    """Build training dataset, possibly removing some background images"""
-    i = 0
-    col = 0
-    ncol = 0
-    bg_count = 0
-    bg_dismiss = 0
-    x_train_list = []
-    y_train_list = []
-    for tile, tag in tiles.values:
-        label = hot_labels[i]
-        i += 1
-        if label[0] == 1:
-          col += 1
-        elif label[1] == 1:
-          ncol += 1
-        else :
-            bg_count += 1
-            if random.uniform(0, 100) <= CConfig.drop_background():
-                bg_dismiss += 1
-                continue
-        x_train_list.append(tile)
-        y_train_list.append(label)
-    print('    - Colonized: {}'.format(col))
-    print('    - Non-colonized: {}'.format(ncol))
-    print('    - Background: {}'.format(bg_count - bg_dismiss))
-    return (x_train_list, y_train_list)
