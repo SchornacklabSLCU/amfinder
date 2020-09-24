d711ccf356395a121d8ba1b8e410d72f0528f289
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-24 06:18:33 +0100

    Removing the need for persistent tapes

--- python-scripts/castanet_activation_mapping.py
+++ python-scripts/castanet_activation_mapping.py
@@ -21,11 +21,13 @@ INVALID_MODEL = 40
 def initialize(model, nrows, ncols):
     """ Initializes the class activation map. """
 
-    global MAPS
-    edge = cConfig.get('tile_edge')
-    blank = np.zeros((nrows * edge, ncols * edge, 3), np.uint8)
-    MAPS = [ blank.copy() for _ in cConfig.get('header')]
-    del blank
+    if cConfig.get('generate_cams'):
+
+        global MAPS
+        edge = cConfig.get('tile_edge')
+        blank = np.zeros((nrows * edge, ncols * edge, 3), np.uint8)
+        MAPS = [ blank.copy() for _ in cConfig.get('header')]
+        del blank
 
 
 
@@ -35,12 +37,13 @@ def get_last_conv_model(model):
     # Uses list reversal. The first Conv2D layer is the last
     # to occur in the given model.
     for layer in reversed(model.layers):
-        if isinstance(layer, Conv2D):
-            last_conv = layer
-            last_conv_model = Model(model.inputs, last_conv.output)
-            return last_conv, last_conv_model 
 
-    # If we reach this, it means <model> has no Conv2D layer!
+        if isinstance(layer, Conv2D):
+
+            last_conv = layer
+            return last_conv, Model(model.inputs, last_conv.output)
+
+    print(f'{model} has no Conv2D layer.')
     sys.exit(INVALID_MODEL)
 
 
@@ -60,16 +63,31 @@ def get_classifier_model(model, last_conv):
 
 
 
-def compute_cam(index, tape, predictions, last_conv_output):
+def compute_cam(index, tile, last_conv_model, classifier_model):
     """ """
 
-    # Builds a tensor class index from a given index,
-    # and determines whether this is the main activation class.
-    class_index = tf.convert_to_tensor(index, dtype='int64') 
-    is_best_match = class_index == tf.argmax(predictions[0])
+    # We add a dimension to transform our array into a batch
+    # of size (1, model_input_size, model_input_size, 3)
+    tile_batch = np.expand_dims(tile, axis=0)
 
-    # Retrieves the corresponding channel.
-    class_channel = predictions[:, class_index]
+    # Compute the last layer output values.
+    last_conv_output = last_conv_model(tile_batch)
+
+    with tf.GradientTape() as tape:
+
+        # Start watching the gradient.
+        tape.watch(last_conv_output)
+
+        # Obtain the predictions.
+        predictions = classifier_model(last_conv_output)
+
+        # Builds a tensor class index from a given index,
+        # and determines whether this is the main activation class.
+        class_index = tf.convert_to_tensor(index, dtype='int64') 
+        is_best_match = class_index == tf.argmax(predictions[0])
+
+        # Retrieves the corresponding channel.
+        class_channel = predictions[:, class_index]
 
     # Retrieves the corresponding gradients.
     grads = tape.gradient(class_channel, last_conv_output)
@@ -119,64 +137,48 @@ def make_heatmap(cam, is_best_match, epsilon=1e-10):
 def generate(model, row, r):
     """ """
 
-    print(f'DEBUG: Processing row {r}')
+    if cConfig.get('generate_cams'):
 
-    edge = cConfig.get('tile_edge')
+        edge = cConfig.get('tile_edge')
 
-    # Maps the input tile to the activations of the last Conv2D layer.
-    last_conv, last_conv_model = get_last_conv_model(model)
+        # Maps the input tile to the activations of the last Conv2D layer.
+        last_conv, last_conv_model = get_last_conv_model(model)
 
-    # Maps the activations of <last_conv> to the final class predictions.
-    classifier_model = get_classifier_model(model, last_conv)
-    
-    for c, tile in enumerate(row):
-
-        # We add a dimension to transform our array into a batch
-        # of size (1, model_input_size, model_input_size, 3)
-        tile_batch = np.expand_dims(tile, axis=0)
-
-        with tf.GradientTape(watch_accessed_variables=False,
-                             persistent=True) as tape:
-
-            # Compute the last layer output values.
-            last_conv_output = last_conv_model(tile_batch)
-
-            # Start watching the gradient.
-            tape.watch(last_conv_output)
-
-            # Obtain the predictions.
-            predictions = classifier_model(last_conv_output)
+        # Maps the activations of <last_conv> to the final class predictions.
+        classifier_model = get_classifier_model(model, last_conv)
+        
+        for c, tile in enumerate(row):
 
             # Generate class activation maps for all annotations classes
             # The function <compute_cam> returns a boolean which indicates
             # whether the given class is the best match.    
-            cams = [compute_cam(i, tape, predictions, last_conv_output)
+            cams = [compute_cam(i, tile, last_conv_model, classifier_model)
                     for i, _ in enumerate(cConfig.get('header'))]
-    
+        
 
-        for (cam, is_best_match), large_map in zip(cams, MAPS):
+            for (cam, is_best_match), large_map in zip(cams, MAPS):
 
-            # Generates the heatmap.
-            heatmap = make_heatmap(cam, is_best_match)
+                # Generates the heatmap.
+                heatmap = make_heatmap(cam, is_best_match)
 
-            # Converts the tile to greyscale.
-            resized = np.uint8(cv2.resize(tile, (edge, edge)) * 255)
-            resized = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
-            resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2BGR)
+                # Converts the tile to greyscale.
+                resized = np.uint8(cv2.resize(tile, (edge, edge)) * 255)
+                resized = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
+                resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2BGR)
 
-            # Overlay the heatmap on the tile.
-            output = np.zeros((edge, edge, 3), np.uint8)
-            output = cv2.addWeighted(heatmap, 0.4, resized, 0.6, 0.0)
-            output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
-            
-            # Inserts the overlay on the large map.
-            rpos = r * edge
-            cpos = c * edge
-            large_map[rpos:rpos + edge, cpos:cpos + edge] = output           
-            
+                # Overlay the heatmap on the tile.
+                output = np.zeros((edge, edge, 3), np.uint8)
+                output = cv2.addWeighted(heatmap, 0.4, resized, 0.6, 0.0)
+                output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+                
+                # Inserts the overlay on the large map.
+                rpos = r * edge
+                cpos = c * edge
+                large_map[rpos:rpos + edge, cpos:cpos + edge] = output           
+
 
 
 def finalize():
     """ """
 
-    return MAPS
+    return MAPS if cConfig.get('generate_cams') else None
