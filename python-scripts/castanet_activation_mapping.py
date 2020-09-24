e607af55b1a9ea95be374f10bd3be0e4d7c85076
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-24 17:35:51 +0100

    Documentation and minor improvements

--- python-scripts/castanet_activation_mapping.py
+++ python-scripts/castanet_activation_mapping.py
@@ -1,5 +1,10 @@
 # CastANet - castanet_activation_mapping.py
 
+""" 
+    Implements the Grad-CAM algorithm
+
+"""
+
 import cv2
 import cmapy
 import keras
@@ -11,85 +16,121 @@ from keras import Model
 from keras.layers import Conv2D
 from keras.layers import Dense
 
+import castanet_log as cLog
 import castanet_config as cConfig
 
 MAPS = []
-INVALID_MODEL = 40
 
 
 
-def initialize(model, nrows, ncols):
-    """ Initializes the class activation map. """
+def initialize(nrows, ncols):
+    """ Create blank images for all annotation classes.
+        PARAMETERS
+        nrows: int
+            Tile row count.
+        ncols: int
+            Tile column count.
+    """
 
     if cConfig.get('generate_cams'):
 
-        global MAPS
+        # Create an empty image, to be used as template.
         edge = cConfig.get('tile_edge')
         blank = np.zeros((nrows * edge, ncols * edge, 3), np.uint8)
-        MAPS = [ blank.copy() for _ in cConfig.get('header')]
-        del blank
+
+        global MAPS
+        
+        # Create copies of the template blank image for each class.
+        MAPS = [blank.copy() for _ in cConfig.get('header')]
 
 
 
 def get_last_conv_model(model):
-    """ Builds a model that connect to the last Conv2D layer. """
+    """ Map the model input to its last convolutional layer.
+        PARAMETERS
+        model: <class 'tensorflow.python.keras.engine.sequential.Sequential'>
+            Pre-trained model used for predictions.
+        RAISES
+        CastANetError
+            if the input model does not contain any convolutional layer.
+    """
 
-    # Uses list reversal. The first Conv2D layer is the last
-    # to occur in the given model.
+    # The last convolutional layer occurs first on the reversed layer list.
     for layer in reversed(model.layers):
 
         if isinstance(layer, Conv2D):
 
             last_conv = layer
-            return last_conv, Model(model.inputs, last_conv.output)
+            last_conv_model = Model(model.inputs, last_conv.output)
 
-    print(f'{model} has no Conv2D layer.')
-    sys.exit(INVALID_MODEL)
+            return (last_conv, last_conv_model)
+
+    # We could not find any convolutional layer in the input model.
+    cLog.failwith(f'{model} has no Conv2D layer', cLog.ERR_INVALID_MODEL)
 
 
 
 def get_classifier_model(model, last_conv):
-    """ Builds a classifier model that connects the last layer output
-        to the successive dense layers and final class predictions. """
+    """ Map the output of the last convolutional layer to model predictions.
+        PARAMETERS
+        model: <class 'tensorflow.python.keras.layers.convolutional.Conv2D'>
+            Pre-trained model used for predictions.
+        last_conv: <class 'tensorflow.python.keras.layers.convolutional.Conv2D'>
+            Last convolutional layer of the given model.
+    """
+
     classifier_input = Input(shape=last_conv.output.shape[1:])
     x = classifier_input
 
-    ok = False # skips all layers until after the last Conv2D.
-    for layer in model.layers:
-        if ok: x = layer(x)
-        if layer.name == last_conv.name: ok = True
+    # Index of the last convolutional layer.
+    last_index = model.layers.index(last_conv)   
+    
+    for layer in model.layers[last_index + 1:]:
+
+        x = layer(x)
     
     return Model(classifier_input, x)
 
 
 
 def compute_cam(index, tile, last_conv_model, classifier_model):
-    """ """
+    """ Compute gradient and class activation map.
+        PARAMETERS
+        index: int
+            Class index.
+        tile: numpy.ndarray
+            Input tile to be processed.
+        last_conv_model: <class 'tensorflow.python.keras.engine.functional.Functional'>
+            Model to retrieve the output of the last Conv2D layer.
+        classifier_model: <class 'tensorflow.python.keras.engine.functional.Functional'>
+            Model to retrieve the gradients.
+    """
 
-    # We add a dimension to transform our array into a batch
-    # of size (1, model_input_size, model_input_size, 3)
+    # Transform the tile array into a batch.
+    # <tile_batch> shape is (1, model_input_size, model_input_size, 3).
     tile_batch = np.expand_dims(tile, axis=0)
 
-    # Compute the last layer output values.
+    # Compute the output of the last convolutional layer.
     last_conv_output = last_conv_model(tile_batch)
 
-    with tf.GradientTape() as tape:
+    with tf.GradientTape(watch_accessed_variables=False) as tape:
 
-        # Start watching the gradient.
+        # Watch the gradient.
         tape.watch(last_conv_output)
 
         # Obtain the predictions.
         predictions = classifier_model(last_conv_output)
 
-        # Builds a tensor class index from a given index,
-        # and determines whether this is the main activation class.
+        # Build a tensor for the given index
         class_index = tf.convert_to_tensor(index, dtype='int64') 
+
+        # Dtermine whether the index corresponds to the best prediction.
         is_best_match = class_index == tf.argmax(predictions[0])
 
-        # Retrieves the corresponding channel.
+        # Retrieve the corresponding channel.
         class_channel = predictions[:, class_index]
 
-    # Retrieves the corresponding gradients.
+    # Retrieve the corresponding gradients.
     grads = tape.gradient(class_channel, last_conv_output)
 
     # Compute the guided gradients.
@@ -97,12 +138,12 @@ def compute_cam(index, tile, last_conv_model, classifier_model):
     cast_grads = tf.cast(grads > 0, 'float32')
     guided_grads = cast_conv_outputs * cast_grads * grads
 
-    # The convolution and guided gradients have a batch dimension
-    # (which we don't need) so let's grab the volume itself.
+    # Remove the unnecessary batch dimension from the convolution 
+    # and from the guided gradients.
     last_conv_output = last_conv_output[0]
     guided_grads = guided_grads[0]
 
-    # Compute the average of the gradient values, and using them
+    # Compute the average of the gradient values, and, using them
     # as weights, compute the ponderation of the filters with
     # respect to the weights.
     weights = tf.reduce_mean(guided_grads, axis=(0, 1))
@@ -112,28 +153,37 @@ def compute_cam(index, tile, last_conv_model, classifier_model):
 
 
 
-def make_heatmap(cam, is_best_match, epsilon=1e-10):
-    """ """
+def make_heatmap(cam, is_best_match, colormap=cv2.COLORMAP_JET):
+    """ Generate a heatmap image from a heatmap tensor.
+        PARAMETERS
+        cam: <class 'tensorflow.python.framework.ops.EagerTensor'>
+            Class activation map tensor.
+        is_best_match: int 
+            Tells whether the CAM corresponds to the best prediction.
+        colormap: int, optional
+            OpenCV colormap to use when <is_best_match> is true.
+    """
 
-    # Resizes the heatmap to input tile size.
+    # Resize the heatmap to input tile size.
     edge = cConfig.get('tile_edge')
     heatmap = cv2.resize(cam.numpy(), (edge, edge))
 
-    # Normalizes heatmap values.
+    # Normalize heatmap values.
     numer = heatmap - np.min(heatmap)
-    denom = heatmap.max() - heatmap.min() + epsilon
+    denom = heatmap.max() - heatmap.min() + 1e-10 # division by zero
     heatmap = numer / denom
     
-    # Converts raw values to 8-bit pixel intensities.
+    # Convert raw values to 8-bit pixel intensities.
     heatmap = (heatmap * 255).astype('uint8')
 
-    # Applies colormap.
-    colormap = cv2.COLORMAP_JET if is_best_match else cv2.COLORMAP_BONE
-    return cv2.applyColorMap(heatmap, colormap)
+    # Apply colormap.
+    colormap = colormap if is_best_match else cv2.COLORMAP_BONE
+    color_heatmap = cv2.applyColorMap(heatmap, colormap)
+
+    return color_heatmap
 
 
 
-# Grad-CAM class activation visualization.
 def generate(model, row, r):
     """ """
 
@@ -141,10 +191,10 @@ def generate(model, row, r):
 
         edge = cConfig.get('tile_edge')
 
-        # Maps the input tile to the activations of the last Conv2D layer.
+        # Map the input tile to the activations of the last Conv2D layer.
         last_conv, last_conv_model = get_last_conv_model(model)
 
-        # Maps the activations of <last_conv> to the final class predictions.
+        # Map the activations of <last_conv> to the final class predictions.
         classifier_model = get_classifier_model(model, last_conv)
         
         for c, tile in enumerate(row):
@@ -158,16 +208,15 @@ def generate(model, row, r):
 
             for (cam, is_best_match), large_map in zip(cams, MAPS):
 
-                # Generates the heatmap.
+                # Generats the heatmap.
                 heatmap = make_heatmap(cam, is_best_match)
 
-                # Converts the tile to greyscale.
+                # Resize the tile to its original size and desaturate.
                 resized = np.uint8(cv2.resize(tile, (edge, edge)) * 255)
-                resized = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
+                resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
                 resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2BGR)
 
-                # Overlay the heatmap on the tile.
-                output = np.zeros((edge, edge, 3), np.uint8)
+                # Overlay the heatmap on top of the desaturated tile.
                 output = cv2.addWeighted(heatmap, 0.4, resized, 0.6, 0.0)
                 output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
                 
@@ -179,6 +228,12 @@ def generate(model, row, r):
 
 
 def finalize():
-    """ """
+    """ Return the class activation map mosaics.
+        RETURNS
+        CAMS
+            if cConfig.get('generate_cams') is set to true.
+        None
+            otherwise.
+    """
 
     return MAPS if cConfig.get('generate_cams') else None
