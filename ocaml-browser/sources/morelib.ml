335bd0a15023fe4ffb051a93ada1806fad884506
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-27 22:48:57 +0100

    Renaming CExt as Morelib

---
+++ ocaml-browser/sources/morelib.ml
@@ -0,0 +1,171 @@
+(* CastANet - morelib.ml *)
+
+(* Operations on string sets. *)
+module StringSet = struct
+
+    open Printf
+
+    let sort str =
+        String.to_seq str
+        |> List.of_seq
+        |> List.sort Char.compare
+        |> List.to_seq
+        |> String.of_seq
+
+    let union s1 s2 =
+        let res = ref s2 in
+        String.iter (fun chr -> 
+            if not (String.contains s2 chr) then
+                res := sprintf "%s%c" !res chr
+        ) s1;
+        sort !res
+
+    let inter s1 s2 =
+        let res = ref "" in
+        String.iter (fun chr ->
+            if String.contains s2 chr then 
+                res := sprintf "%s%c" !res chr
+        ) s1;
+        sort !res
+
+    let diff s1 s2 =
+        let res = ref "" in 
+        String.iter (fun chr ->
+            if not (String.contains s2 chr) then
+                res := sprintf "%s%c" !res chr
+        ) s1;
+        sort !res
+
+end
+
+
+
+module File = struct
+    let read ?(binary = false) ?(trim = true) str = 
+        let ich =
+            match binary with
+            | true -> open_in_bin str
+            | false -> open_in str in
+        let len = in_channel_length ich in
+        let buf = Buffer.create len in
+        Buffer.add_channel buf ich len;
+        close_in ich;
+        let str = Buffer.contents buf in
+        if trim then String.trim str else str
+end
+
+external id : 'a -> 'a = "%identity"
+
+
+
+module Matrix = struct
+
+    type 'a t = 'a Array.t Array.t
+
+    let dim t =
+        let r = length t in
+        if r = 0 then (0, 0) else r, length t.(0)
+
+    let get_opt t ~r ~c = try Some t.(r).(c) with _ -> None
+
+    let get t ~r ~c =
+        match get_opt t ~r ~c with
+        | None -> invalid_arg "CExt.Matrix.get: Out of range"
+        | Some x -> x
+
+    let set t ~r ~c x = t.(r).(c) <- x
+
+    let make ~r ~c x = Array.init r (fun _ -> Array.make c x)
+    let init ~r ~c f = Array.init r (fun r -> Array.init c (fun c -> f ~r ~c))
+
+    let map f = Array.map (Array.map f)
+    let mapi f = Array.mapi (fun r -> Array.mapi (fun c -> f ~r ~c))
+
+    let iter f = Array.iter (Array.iter f)
+    let iteri f = Array.iteri (fun r -> Array.iteri (fun c -> f ~r ~c))
+
+    let fold f ini t = 
+        let res = ref ini in
+        Array.iteri (fun r -> Array.iteri (fun c x -> res := f ~r ~c !res x)) t;
+        !res
+
+    let copy ?(dat = id) mat = Array.map (Array.map dat) mat
+
+    let to_string ~cast t =
+        Array.map (Array.map cast) t
+        |> Array.map to_list
+        |> Array.map (String.concat "\t")
+        |> Array.to_list
+        |> String.concat "\n"
+
+    let to_string_rc ~cast t =
+        Array.mapi (fun r -> (Array.mapi (fun c -> cast ~r ~c))) t
+        |> Array.map to_list
+        |> Array.map (String.concat "\t")
+        |> Array.to_list
+        |> String.concat "\n"
+
+    let of_string ~cast s =
+        String.split_on_char '\n' s
+        |> Array.of_list
+        |> Array.map (String.split_on_char '\t')
+        |> Array.map of_list
+        |> Array.map (Array.map cast)
+
+    let of_string_rc ~cast s =
+        String.split_on_char '\n' s
+        |> Array.of_list
+        |> Array.map (String.split_on_char '\t')
+        |> Array.map of_list
+        |> Array.mapi (fun r -> (Array.mapi (fun c -> cast ~r ~c)))
+
+end
+
+
+
+module Text = struct
+
+    open String
+    
+    let explode s = List.init (length s) (get s)
+    let implode t = concat "" (List.map (String.make 1) t)
+end
+
+
+(*
+let time f x =
+  let t_1 = Unix.gettimeofday () in
+  let res = f x in
+  let t_2 = Unix.gettimeofday () in
+  CLog.info "Elapsed time: %.1f s" (t_2 -. t_1);
+  res
+*)
+
+
+(* Memoized values with possible reinitialization. *)
+module Memoize = struct
+  let flag = ref 0 (* Main flag. *)
+
+  type 'a t = {
+    mutable flag : int; (* Local flag. *)
+    mutable data : [`F of (unit -> 'a) | `R of 'a]; (* Memoized data. *)
+  }
+
+  let exec f mem = let x = f () in mem.data <- `R x; x
+
+  let create ?label ?(one = false) f =
+    let mem = { flag = if one then -1 else 0; data = `F f } in
+    (fun () -> match mem.data with
+      | `F f -> exec f mem (* Runs the function, store and return result. *)
+      | `R x -> 
+        if mem.flag >= 0 && !flag > mem.flag then (
+          (* The main flag has changed: recompute, store and return result. *)
+          Gaux.may (CLog.info "Recomputing data for '%s'") label;
+          let x = exec f mem in 
+          mem.flag <- mem.flag + 1;
+          x
+        ) else x (* Return the previously computed result. *)
+    )
+
+  let forget () = incr flag
+end
---
+++ ocaml-browser/sources/morelib.mli
@@ -0,0 +1,114 @@
+(* CastANet - morelib.mli *)
+
+(** Lightweight extension of the OCaml standard library. *)
+
+(** String sets. *)
+module StringSet : sig
+    val union : string -> string -> string
+    (** [union s1 s2] returns a string containing one instance of all characters
+      * occurring in either [s1] or [s2], sorted in alphabetical order.  *)
+
+    val inter : string -> string -> string
+    (** [union s1 s2] returns a string containing one instance of all characters
+      * occurring in both [s1] or [s2], sorted in alphabetical order.  *)
+
+    val diff : string -> string -> string
+    (** [union s1 s2] returns a string containing one instance of all characters
+      * occurring in [s1] but not in [s2], sorted in alphabetical order.  *)
+end
+
+
+(** Matrix iterators. *)
+module Matrix : sig
+    type 'a t = 'a array array
+    (** The type of matrices. *)
+
+    val dim : 'a t -> int * int
+    (** Returns the the number of rows and columns of the given matrix. *)
+
+    val get : 'a t -> r:int -> c:int -> 'a
+    (** [get t ~r ~c] returns [t.(r).(c)].
+      * @raise Invalid_argument if the given indices are out of range. *)
+
+    val get_opt : 'a t -> r:int -> c:int -> 'a option
+    (** Same as [get], but returns [None] if indices are invalid. *)
+
+    val set : 'a t -> r:int -> c:int -> 'a -> unit
+    (** [set t ~r ~c x] stores [x] at row [r] and column [c] of matrix [t]. *)
+
+    val make : r:int -> c:int -> 'a -> 'a t
+    (** [make ~r ~c x] returns a matrix containing [r] rows and [c] columns,
+      * with all elements initialized with [x]. *)
+
+    val init : r:int -> c:int -> (r:int -> c:int -> 'a) -> 'a t
+    (** [init ~r ~c f] builds a matrix of [r] rows and [c] columns and initializes
+    * values using the function [f]. *)
+
+    val map : ('a -> 'b) -> 'a t -> 'b t
+    (** [map f m] builds a new matrix by applying [f] to all members of [m]. *)
+
+    val mapi : (r:int -> c:int -> 'a -> 'b) -> 'a t -> 'b t
+    (** Same as [map], but [f] receives row and column indexes as parameters. *)
+
+    val iter : ('a -> unit) -> 'a t -> unit
+    (** [iter f m] applies [f] to all members of the matrix [m]. *)
+
+    val iteri : (r:int -> c:int -> 'a -> unit) -> 'a t -> unit
+    (** Same as [iter], but oasses row and column indexes as parameters. *)
+
+    val fold : (r:int -> c:int -> 'a -> 'b -> 'a) -> 'a -> 'b t -> 'a
+    (** Fold function. *)
+
+    val to_string : cast:('a -> string) -> 'a t -> string
+    (** Exports a matrix as string. *)
+
+    val to_string_rc : cast:(r:int -> c:int -> 'a -> string) -> 'a t -> string
+    (** Same as [to_string], but passes row and column indexes to [cast]. *)
+
+    val of_string : cast:(string -> 'a) -> string -> 'a t
+    (** Imports a matrix from a string. *)
+    
+    val of_string_rc : cast:(r:int -> c:int -> string -> 'a) -> string -> 'a t
+    (** Same as [of_string], but passes row and column indexes to [cast]. *)
+
+    val copy : ?dat:('a -> 'a) -> 'a t -> 'a t
+    (** Returns a copy of a given matrix. *)
+end
+
+
+(** Text operations. *)
+module Text : sig
+  val explode : string -> char list
+  (** Converts a string into a list of characters. *)
+
+  val implode : char list -> string
+  (** Converts a list of characters into a string. *)
+end
+
+
+(** Operations on files. *)
+module File : sig
+  val read : ?binary:bool -> ?trim:bool -> string -> string
+  (** Reads a file. Setting up option [binary] results in file being opened in
+    * binary mode (default: [false]). Option [trim] triggers trimming of 
+    * leading and trailing spaces (default: [true]). *)
+end
+
+
+val time : ('a -> 'b) -> 'a -> 'b
+(** Benchmark function. *)
+
+(** Memoized values with possible reinitialization. Such values are computed 
+  * only once within a given session. However, they can recomputed when a new 
+  * session starts, for instance to take into account the modification of 
+  * general settings that affect the computation of memoized values. *)
+module Memoize : sig
+  val create : ?label:string -> ?one:bool -> (unit -> 'a) -> unit -> 'a
+  (** Memoization function. The optional parameter [lbl] is used to report when
+    * data are being recomputed due to a previous call to [forget] (see below).
+    * The optional parameter [one] defines memoized values that are never
+    * recomputed, i.e. insensitive to [forget] invokation. *)
+
+  val forget : unit -> unit
+  (** Triggers all memoized data to be recomputed when accessed. *)
+end
