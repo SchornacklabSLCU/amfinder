3951a33ea500c86d43c51ba6d9b953b82baf825d
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-27 10:57:20 +0100

    Code cleaning up and refactoring

--- ocaml-browser/sources/cLevel.ml
+++ ocaml-browser/sources/cLevel.ml
@@ -2,55 +2,68 @@
 
 open CExt
 
+
+
 type t = [
-  | `COLONIZATION (* colonized vs non-colonized vs background                 *)
-  | `ARB_VESICLES (* [arbuscules, vesicles] vs non-colonized vs background    *)
-  | `ALL_FEATURES (* [ERH, [root, [arb, ves, IRH, hyphopodia]]] vs background *)
+    | `COLONIZATION
+    | `ARB_VESICLES
+    | `ALL_FEATURES
 ]
 
-let flags = [`COLONIZATION; `ARB_VESICLES; `ALL_FEATURES]
-let strings = ["Colonization"; "Arbuscules/Vesicles"; "All features"]
 
+
+let to_string = function
+    | `COLONIZATION -> "colonization"
+    | `ARB_VESICLES -> "arb_vesicles"
+    | `ALL_FEATURES -> "all_features"
+
+
+
+let of_string str =
+    match String.lowercase_ascii str with
+    | "colonization" -> `COLONIZATION
+    | "arb_vesicles" -> `ARB_VESICLES
+    | "all_features" -> `ALL_FEATURES
+    | _               -> invalid_arg "(CLevel.of_string) Wrong annotation level" 
+
+
+
+let to_header = function
+    | `COLONIZATION -> ['Y'; 'N'; 'X']
+    | `ARB_VESICLES -> ['A'; 'V'; 'N'; 'X']
+    | `ALL_FEATURES -> ['A'; 'V'; 'E'; 'I'; 'H'; 'R'; 'X']
+
+
+
+let of_header t =
+    match List.map Char.uppercase_ascii t with
+    | ['Y'; 'N'; 'X'] -> `COLONIZATION
+    | ['A'; 'V'; 'N'; 'X'] -> `ARB_VESICLES
+    | ['A'; 'V'; 'E'; 'I'; 'H'; 'R'; 'X'] -> `ALL_FEATURES
+    | _ -> invalid_arg "(CLevel.of_header) Malformed table header"
+
+
+
+let all_flags = [`COLONIZATION; `ARB_VESICLES; `ALL_FEATURES]
 let lowest = `COLONIZATION
 let highest = `ALL_FEATURES
 
-let others = function
-  | `COLONIZATION -> [`ARB_VESICLES; `ALL_FEATURES]
-  | `ARB_VESICLES -> [`COLONIZATION; `ALL_FEATURES]
-  | `ALL_FEATURES -> [`COLONIZATION; `ARB_VESICLES]
 
-module Colors = struct
-  let colonization = ["#80b3ff"; "#bec8b7"; "#ffaaaa"]
-  let arb_vesicles = ["#80b3ff"; "#afe9c6"; "#bec8b7"; "#ffaaaa"]
-  let all_features = ["#80b3ff"; "#afe9c6"; "#ffeeaa"; "#eeaaff";
-                      "#ffb380"; "#bec8b7"; "#ffaaaa" ]
-end
+
+let others = function
+    | `COLONIZATION -> [`ARB_VESICLES; `ALL_FEATURES]
+    | `ARB_VESICLES -> [`COLONIZATION; `ALL_FEATURES]
+    | `ALL_FEATURES -> [`COLONIZATION; `ARB_VESICLES]
+
+
 
 let colors = function
-  | `COLONIZATION -> Colors.colonization
-  | `ARB_VESICLES -> Colors.arb_vesicles
-  | `ALL_FEATURES -> Colors.all_features  
+    | `COLONIZATION -> ["#80b3ff"; "#bec8b7"; "#ffaaaa"]
+    | `ARB_VESICLES -> ["#80b3ff"; "#afe9c6"; "#bec8b7"; "#ffaaaa"]
+    | `ALL_FEATURES -> ["#80b3ff"; "#afe9c6"; "#ffeeaa"; "#eeaaff";
+                        "#ffb380"; "#bec8b7"; "#ffaaaa" ]
 
-let of_list t =
-  let header =
-    List.map Char.uppercase_ascii t
-    |> List.sort Char.compare
-    |> List.map (String.make 1)
-    |> String.concat ""
-  in
-  match header with
-  | "NXY" -> `COLONIZATION
-  | "ANVX" -> `ARB_VESICLES
-  | "AEHIRVX" -> `ALL_FEATURES
-  | _ -> CLog.error "Invalid header %S (CLevel.of_list)" header
 
-let to_string = function
-  | `COLONIZATION -> "`COLONIZATION"
-  | `ARB_VESICLES -> "`ARB_VESICLES"
-  | `ALL_FEATURES -> "`ALL_FEATURES"
+(* let strings = ["Colonization"; "Arbuscules/Vesicles"; "All features"] *)
 
-let of_string = function
-  | "`COLONIZATION" -> `COLONIZATION
-  | "`ARB_VESICLES" -> `ARB_VESICLES
-  | "`ALL_FEATURES" -> `ALL_FEATURES
-  | _               -> invalid_arg "CLevel.of_string" 
+
--- ocaml-browser/sources/cLevel.mli
+++ ocaml-browser/sources/cLevel.mli
@@ -9,30 +9,41 @@ type t = [
 ]
 (** Annotation levels. *)
 
-val flags : t list
+
+val to_string : t -> string
+(** [to_string t] returns the textual representation of the level [t]. *)
+
+
+val of_string : string -> t
+(** [of_string s] returns the level corresponding to the string [s]. *)
+
+
+val to_header : t -> char list
+(** [to_header t] returns the header associated with annotation level [t]. *)
+
+
+val of_header : char list -> t
+(** [of_header t] returns the annotation level associated with header [t]. *)
+
+
+val all_flags : t list
 (** List of available annotation levels, sorted from lowest to highest. *)
 
-val strings : string list
-(** String representation of the available annotations levels, sorted from
-  * lowest to highest. *)
 
 val lowest : t
 (** Least detailed level of mycorrhiza annotation. *)
 
+
 val highest : t
 (** Most detailed level of mycorrhiza annotation. *)
 
+
 val others : t -> t list
-(** [other lvl] returns the two other levels than [lvl], sorted from lowest to
-  * highest. *)
+(** [other t] returns the list of all annotations levels but [t]. *)
+
 
 val colors : t -> string list
-(** Returns the list of colors at the given level. *)
+(** [colors t] returns the list of RGB colors to use to display annotations at
+  * level [t]. *)
 
-val to_string : t -> string
 
-val of_string : string -> t
-
-val of_list : char list -> t
-(** [of_list t] returns the level corresponding to the header [t]. The function
-  * is case-insensitive and characters can be given in any order. *)
