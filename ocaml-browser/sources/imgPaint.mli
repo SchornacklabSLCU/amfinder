dc39146017ae7083f8b47b0ac55395334caf99cc
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-29 08:39:05 +0100

    Renaming ImgDraw as ImgPaint

---
+++ ocaml-browser/sources/imgPaint.mli
@@ -0,0 +1,56 @@
+(* CastANet Browser - imgDraw.mli *)
+
+(** Image drawing functions. *)
+
+class type t = object
+
+    method edge : int
+    (** Returns tile size. *)
+
+    method x_origin : int
+    (** Returns drawing origin on X axis. *)
+
+    method y_origin : int
+    (** Returns drawing origin on Y axis. *)
+
+    method cursor_pos : int * int
+    (** Returns cursor position. *)
+
+    method set_cursor_pos : (int * int) -> unit
+    (** Defines cursor position. *)
+
+    method backcolor : string
+    (** Returns image background color. *)
+
+    method set_backcolor : string -> unit
+    (** Defines image background color. *)
+
+    method background : ?sync:bool -> unit -> unit
+    (** Draws a white background on the right image area.
+      * @param sync defaults to [true]. *)
+
+    method pixbuf : ?sync:bool -> r:int -> c:int -> GdkPixbuf.pixbuf -> unit
+    (** [pixbuf ?sync ~r ~c p] draws pixbuf [p] at row [r] and column [c].
+      * @param sync defaults to [false]. *)
+
+    method surface : ?sync:bool -> r:int -> c:int -> Cairo.Surface.t -> unit
+    (** [surface ?sync ~r ~c s] draws surface [s] at row [r] and column [c].
+      * @param sync defaults to [false]. *)
+
+    method cursor : ?sync:bool -> r:int -> c:int -> unit -> unit
+    (** Draws the cursor.
+      * @param sync defaults to [false]. *)
+
+    method pointer : ?sync:bool -> r:int -> c:int -> unit -> unit
+    (** Draws the cursor at mouse pointer.
+      * @param sync defaults to [false]. *)
+
+    method annotation : ?sync:bool -> r:int -> c:int -> CLevel.t -> char -> unit
+    (** Draws a tile annotation.
+      * @param sync defaults to [false]. *)
+
+end
+
+
+val create : ImgSource.t -> t
+(** Creates drawing functions. *)
