f96a099ba2dd59e250ced42b215c2d510d33d644
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-29 20:23:21 +0100

    Renaming UI modules

--- ocaml-browser/sources/uI_Helper.mli
+++
@@ -1,7 +0,0 @@
-(* CastANet - uI_Helper.ml *)
-
-val separator : (GButton.tool_item_o -> unit) -> unit
-
-val morespace : (GButton.tool_item_o -> unit) -> unit
-
-val label : ?vspace:bool -> (GButton.tool_item_o -> unit) -> string -> GMisc.label
