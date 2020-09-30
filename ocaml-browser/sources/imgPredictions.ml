35d56b2b66aceade593d039fa5ed5bd707445d49
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-09-30 19:32:24 +0100

    New method max_layer, various improvements, and documentation

--- ocaml-browser/sources/imgPredictions.ml
+++ ocaml-browser/sources/imgPredictions.ml
@@ -21,48 +21,60 @@ module Aux = struct
         List.iter (fun ((r, c), t) -> table.(r).(c) <- t) raw;
         table
 
-    let to_string data =
+    let to_string level table =
         let buf = Buffer.create 100 in
         (* TODO: improve this! *)
-        let header = match List.length data.(0).(0) with
-            | 3 -> "Y\tN\tX"
-            | 4 -> "A\tV\tN\tX"
-            | 7 -> "A\tV\tE\tI\tH\tR\tX"
-            | _ -> invalid_arg "ImgPredictions.Aux.to_string: Unknown header" in
+        let header = CLevel.to_header level
+            |> List.map (String.make 1)
+            |> String.concat "\t" in
         bprintf buf "row\tcol\t%s\n" header;
         Matrix.iteri (fun ~r ~c t ->
             List.map Float.to_string t
             |> String.concat "\t"
             |> bprintf buf "%d\t%d\t%s\n" r c
-        ) data;
+        ) table;
         Buffer.contents buf
 end
 
 
-class predictions assoc_table = object
+class predictions input = object (self)
 
     val mutable curr : string option = 
-        match assoc_table with
+        match input with
         | [] -> None (* TODO: Find a better solution to this! *)
         | (id, _) :: _ -> Some id
 
     method current = curr
     method set_current x = curr <- Some x
 
-    method get ~r ~c =
-        match curr with
+    method private current_data = Option.map (fun x -> List.assoc x input) curr
+    method private table = Option.map (fun (_, y) -> y) self#current_data
+    method private level = Option.map (fun (x, _) -> x) self#current_data
+
+    method get ~r ~c = 
+        match self#table with
         | None -> None
-        | Some x -> Matrix.get_opt (List.assoc x assoc_table) ~r ~c
+        | Some t -> Matrix.get_opt t ~r ~c
+
+    method max_layer ~r ~c =
+        match self#current_data with
+        | None -> None
+        | Some (level, table) ->
+            Option.map (fun preds ->
+                fst @@ List.fold_left2 (fun ((_, x) as z) y chr ->
+                    if y > x then (chr, y) else z
+                ) ('0', 0.0) preds (CLevel.to_header level)
+            ) (Matrix.get_opt table ~r ~c)
 
     method iter f =
-        match curr with
+        match self#table with
         | None -> ()
-        | Some x -> Matrix.iteri f (List.assoc x assoc_table)
+        | Some matrix -> Matrix.iteri f matrix
 
     method to_string () = 
-        match curr with
+        match self#current_data with
         | None -> "" (* TODO: Find a better solution to this! *)
-        | Some x -> Aux.to_string (List.assoc x assoc_table)
+        | Some (level, table) -> Aux.to_string level table
 
 end
 
@@ -72,6 +84,12 @@ let filter entries =
         Filename.dirname filename = "predictions"
     ) entries
 
+let level_of_filename s =
+    String.split_on_char '.' s
+    |> List.rev
+    |> List.hd
+    |> CLevel.of_string
+
 
 let create ?zip source =
     match zip with
@@ -79,9 +97,10 @@ let create ?zip source =
     | Some ich -> let entries = Zip.entries ich in
         let assoc =
             List.map (fun ({Zip.filename; _} as entry) ->
+                let level = level_of_filename filename in
                 let matrix = Aux.of_string (Zip.read_entry ich entry)
                 and id = Filename.(basename (chop_extension filename)) in
-                id, matrix
+                id, (level, matrix)
             ) (filter entries)
         in new predictions assoc
 
--- ocaml-browser/sources/imgPredictions.mli
+++ ocaml-browser/sources/imgPredictions.mli
@@ -1,11 +1,25 @@
 (* CastANet browser - imgPredictions.mli *)
 
+(** Prediction tables. *)
+
 class type predictions = object
     method current : string option
+    (** Return the identifier of the active prediction table. *)
+
     method set_current : string -> unit
-    method get : r:int -> c:int -> float list option 
+    (** Define the active prediction table. *)
+
+    method get : r:int -> c:int -> float list option
+    (** Return the probabilities at the given coordinates. *)
+    
+    method max_layer : r:int -> c:int -> char option
+    (** Return the layer with maximum probability at the given coordinates. *)
+    
     method iter : (r:int -> c:int -> float list -> unit) -> unit
+    (** Iterate over all predictions. *)
+    
     method to_string : unit -> string
+    (** Return the string representation of the active prediction table. *)
 end
 
 val create : ?zip:Zip.in_file -> ImgSource.source -> predictions
