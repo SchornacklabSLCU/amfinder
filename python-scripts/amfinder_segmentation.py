385f7f278b9805cfa890d8e66e7ccc087aa6c430
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-12-10 10:02:45 +0000

    Documentation

--- python-scripts/amfinder_segmentation.py
+++ python-scripts/amfinder_segmentation.py
@@ -1,4 +1,4 @@
-# CastANet - amfinder_segmentation.py
+# AMFinder - amfinder_segmentation.py
 
 import random
 import pyvips
@@ -7,20 +7,22 @@ import numpy as np
 
 import amfinder_config as cConfig
 
+# Interpolation mode, optimized for speed.
 INTERP_NEAREST = pyvips.vinterpolate.Interpolate.new('nearest')
-
-
 
 def tile(image, r, c):
     """ Extracts a tile from a large image, resizes it to
-        model input size, and returns it as a NumPy array. """
+        model input size, and returns it as a numpy array. """
 
+    # Retrieves an individual tile.
     edge = cConfig.get('tile_edge')
-
     tile = image.crop(c * edge, r * edge, edge, edge)
-    tile = tile.resize(cConfig.get('model_input_size') / edge,
-                       interpolate=INTERP_NEAREST)
 
-    return np.ndarray(buffer=tile.write_to_memory(),
+    # Resizes to N x N pixels, N = 126 = cConfig.get('model_input_size')
+    ratio = cConfig.get('model_input_size') / edge
+    resized = tile.resize(ratio, interpolate=INTERP_NEAREST)
+
+    # Returns as numpy array.
+    return np.ndarray(buffer=resized.write_to_memory(),
                       dtype=np.uint8,
-                      shape=[tile.height, tile.width, tile.bands])
+                      shape=[resized.height, resized.width, resized.bands])
