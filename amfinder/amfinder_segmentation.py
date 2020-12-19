9881a48fb3110eb53097be22601e7c655cd4541b
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-12-19 22:17:02 +0000

    Bug fixes, and documentation

--- amfinder/amfinder_segmentation.py
+++ amfinder/amfinder_segmentation.py
@@ -1,26 +1,41 @@
 # AMFinder - amfinder_segmentation.py
 
+""" Image Segmentation.
+
+    Returns tiles of a large input image.
+    
+    Constants
+    -----------
+    INTERPOLATION - Interpolation mode for image resizing.
+
+    Functions
+    ------------
+    tile - crops a tile at the given coordinates within a large input image.
+"""
+
 import pyvips
 import numpy as np
-import amfinder_config as cConfig
 
-# Interpolation mode, optimized for speed.
-INTERP_NEAREST = pyvips.vinterpolate.Interpolate.new('nearest')
+import amfinder_model as AmfModel
+import amfinder_config as AmfConfig
+
+
+
+# Not the best quality, but optimized for speed.
+INTERPOLATION = pyvips.vinterpolate.Interpolate.new('nearest')
+
 
 
 def tile(image, r, c):
     """ Extracts a tile from a large image, resizes it to
         model input size, and returns it as a numpy array. """
 
-    # Retrieves an individual tile.
-    edge = cConfig.get('tile_edge')
+    edge = AmfConfig.get('tile_edge')
     tile = image.crop(c * edge, r * edge, edge, edge)
 
-    # Resizes to N x N pixels, N = 126 = cConfig.get('model_input_size')
-    ratio = 126 / edge
-    resized = tile.resize(ratio, interpolate=INTERP_NEAREST)
+    ratio = AmfModel.INPUT_SIZE / edge
+    resized = tile.resize(ratio, interpolate=INTERPOLATION)
 
-    # Returns as numpy array.
     return np.ndarray(buffer=resized.write_to_memory(),
                       dtype=np.uint8,
                       shape=[resized.height, resized.width, resized.bands])
