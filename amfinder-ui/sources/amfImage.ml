3c2e6a41fe3b123f8e88e50344a8d9f9192e8dfb
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2020-12-22 13:14:24 +0000

    License header

--- amfinder-ui/sources/amfImage.ml
+++ amfinder-ui/sources/amfImage.ml
@@ -1,4 +1,26 @@
-(* The Automated Mycorrhiza Finder version 1.0 - amfImage.ml *)
+(* AMFinder - amfImage.ml
+ *
+ * MIT License
+ * Copyright (c) 2021 Edouard Evangelisti
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *)
 
 open Printf
 
@@ -241,7 +263,7 @@ object (self)
                         if mask#mem 'Y' then Some "#FF00FFB0" else
                         if mask#mem 'N' then Some "#707070B0" else None in
                     Option.iter (fun clr ->
-                        let s = AmfSurface.Square.filled clr e in
+                        let s = AmfSurface.Annotation.filled clr e in
                         Cairo.set_source_surface t s ~x ~y;
                         Cairo.paint t
                     ) opt    
