ae4c7d1445592f6bd0449ce834f9fb9fc55d010c
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2021-03-10 09:42:12 +0000

    Removing old documentation

--- doc/graph/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd ../../ocaml-browser/sources
-
-ocamldep -one-line -I img -I ui *.ml *.mli \
-  | grep -v "cmx" \
-  | ../../doc/graph/mkgraph.py \
-  | tee ../../doc/graph/dep_graph.dot \
-  | dot -x -Tpng > ../../doc/graph/dep_graph.png
