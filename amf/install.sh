83d31b382699366d703b5f1abfea08aa7d661b8d
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2021-04-27 13:47:09 +0100

    Improving the installation procedure

--- amf/install.sh
+++ amf/install.sh
@@ -7,4 +7,5 @@ $PYTHON -m venv amfenv && \
 source amfenv/bin/activate && \
 python -m pip install --upgrade pip && \
 python -m pip install -r requirements.txt && \
+deactivate && \
 echo "The AMFinder tool <amf> was successfully installed."
