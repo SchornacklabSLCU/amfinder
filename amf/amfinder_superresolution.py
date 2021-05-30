62849987e6c17907209586dd751435d20e0c82cd
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2021-05-30 18:19:31 +0100

    Various improvements and optimisations of SRGAN

--- amf/amfinder_superresolution.py
+++ amf/amfinder_superresolution.py
@@ -48,13 +48,9 @@ CHANNELS = 3
 LR_SHAPE = (LR_EDGE, LR_EDGE, CHANNELS)
 HR_SHAPE = (HR_EDGE, HR_EDGE, CHANNELS)
 
-SAMPLE_INTERVAL = 20
 PATCH = 8 #int(HR_EDGE / 2**4) # 7
 DISC_PATCH = (PATCH, PATCH, 1)
 OPTIMIZER = Adam(0.0002, 0.5)
-RESIDUAL_BLOCKS = 16
-GFILTERS = 64 # Generator filters
-DFILTERS = 64 # Discriminator filters
 
 
 #INDEX=0
@@ -124,56 +120,59 @@ def d_block(layer_input, filters, strides=1, batch_norm=True):
 
 
 def build_discriminator():
+    
+    df = 64
+
     d0 = Input(shape=HR_SHAPE)
-    d1 = d_block(d0, DFILTERS, batch_norm=False)
-    d2 = d_block(d1, DFILTERS, strides=2)
-    d3 = d_block(d2, DFILTERS * 2)
-    d4 = d_block(d3, DFILTERS * 2, strides=2)
-    d5 = d_block(d4, DFILTERS * 4)
-    d6 = d_block(d5, DFILTERS * 4, strides=2)
-    d7 = d_block(d6, DFILTERS * 8)
-    d8 = d_block(d7, DFILTERS * 8, strides=2)
-    d9 = Dense(DFILTERS * 16)(d8)
+    d1 = d_block(d0, df, batch_norm=False)
+    d2 = d_block(d1, df, strides=2)
+    d3 = d_block(d2, df * 2)
+    d4 = d_block(d3, df * 2, strides=2)
+    d5 = d_block(d4, df * 4)
+    d6 = d_block(d5, df * 4, strides=2)
+    d7 = d_block(d6, df * 8)
+    d8 = d_block(d7, df * 8, strides=2)
+    d9 = Dense(df * 16)(d8)
     d10 = LeakyReLU(alpha=0.2)(d9)
     validity = Dense(1, activation='sigmoid')(d10)
     return Model(d0, validity, name='discriminator')
 
 
 
-def residual_block(layer_input, filters):
+def residual_block(layer_input):
     """Residual block described in paper"""
-    d = Conv2D(filters, kernel_size=3, strides=1, padding='same')(layer_input)
-    d = Activation('relu')(d)
-    d = BatchNormalization(momentum=0.8)(d)
-    d = Conv2D(filters, kernel_size=3, strides=1, padding='same')(d)
-    d = BatchNormalization(momentum=0.8)(d)
+    d = Conv2D(64, kernel_size=3, strides=1, padding='same')(layer_input)
+    d = BatchNormalization(momentum=0.5)(d)
+    d = PReLU(shared_axes = [1,2])(d)
+    d = Conv2D(64, kernel_size=3, strides=1, padding='same')(d)
+    d = BatchNormalization(momentum=0.5)(d)
     d = Add()([d, layer_input])
     return d
 
 
 
-def deconv2d(layer_input):
+def upscale_block(layer_input):
     """Layers used during upsampling"""
-    u = UpSampling2D(size=3)(layer_input)
-    u = Conv2D(256, kernel_size=3, strides=1, padding='same')(u)
-    u = Activation('relu')(u)
+    u = Conv2D(256, kernel_size=3, strides=1, padding='same')(layer_input)
+    u = UpSampling2D(size=3)(u)
+    u = PReLU(shared_axes=[1,2])(u)
     return u
 
 
 
-def build_generator():
+def build_generator(residual_blocks=16):
 
     # Low resolution image input
     img_lr = Input(shape=LR_SHAPE)
 
     # Pre-residual block
     c1 = Conv2D(64, kernel_size=9, strides=1, padding='same')(img_lr)
-    c1 = Activation('relu')(c1)
+    c1 = PReLU(shared_axes=[1,2])(c1)
 
     # Propogate through residual blocks
-    r = residual_block(c1, GFILTERS)
-    for _ in range(RESIDUAL_BLOCKS - 1):
-        r = residual_block(r, GFILTERS)
+    r = residual_block(c1)
+    for _ in range(residual_blocks - 1):
+        r = residual_block(r)
 
     # Post-residual block
     c2 = Conv2D(64, kernel_size=3, strides=1, padding='same')(r)
@@ -181,15 +180,13 @@ def build_generator():
     c2 = Add()([c2, c1])
 
     # Upsampling
-    #u1 = deconv2d(c2)
-    u2 = deconv2d(c2)
+    u2 = upscale_block(c2)
 
-    # Generate high resolution output
+    # Generate high resolution output (activation='tanh')
     gen_hr = Conv2D(CHANNELS, 
                     kernel_size=9,
                     strides=1,
-                    padding='same',
-                    activation='tanh')(u2)
+                    padding='same')(u2)
 
     return Model(img_lr, gen_hr, name='generator')
 
@@ -217,7 +214,7 @@ def get_random_tile_pairs(tiles, batch_size=1, training=True):
 
 
 
-def train(paths):
+def train(paths, batch_size=50, sample_interval=20):
 
     print('  - Building feature extractor')
    
@@ -262,14 +259,18 @@ def train(paths):
     # Discriminator determines validity of generated high res. images
     validity = frozen_discriminator(fake_hr)
 
-    adversarial_model = Model([img_lr, img_hr], [validity, fake_features])
+    gan_model = Model(inputs=[img_lr, img_hr],
+                      outputs=[validity, fake_features],
+                      name='SRGAN')
 
-    adversarial_model.compile(loss=['binary_crossentropy', 'mse'],
-                              loss_weights=[1e-3, 1],
-                              optimizer=OPTIMIZER)
+    gan_model.summary()
+
+    gan_model.compile(loss=['binary_crossentropy', 'mse'],
+                      loss_weights=[1e-3, 1],
+                      optimizer=OPTIMIZER)
                     
     tiles = get_tiles(paths)
-    epochs = len(tiles) * 100
+    epochs = len(tiles) * 200
 
     start_time = datetime.datetime.now()
 
@@ -277,14 +278,14 @@ def train(paths):
 
         print('Epoch {}/{}... '.format(epoch + 1, epochs), end='')
 
-        hr_tile, lr_tile = get_random_tile_pairs(tiles)
+        hr_tile, lr_tile = get_random_tile_pairs(tiles, batch_size=batch_size)
         
         # From low res. image generate high res. version
         # (N, LR_EDGE * 4, LR_EDGE * 4, CHANNELS)
         fake_hr = generator.predict(lr_tile)
 
-        valid = np.ones((1,) + DISC_PATCH)
-        fake = np.zeros((1,) + DISC_PATCH)
+        valid = np.ones((batch_size,) + DISC_PATCH)
+        fake = np.zeros((batch_size,) + DISC_PATCH)
 
         # Train the discriminators
         # (original images = real / generated = Fake)
@@ -292,25 +293,32 @@ def train(paths):
         d_loss_fake = discriminator.train_on_batch(fake_hr, fake)
         d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
-        hr_tile, lr_tile = get_random_tile_pairs(tiles)
+        hr_tile, lr_tile = get_random_tile_pairs(tiles, batch_size=batch_size)
 
-        # The generators want the discriminators to label the generated images as real
-        valid = np.ones((1,) + DISC_PATCH)
+        # The generators want the discriminators 
+        # to label the generated images as real
+        #valid = np.ones((batch_size, 1))
 
         # Extract ground truth image features using pre-trained CNN1 model
         image_features = feature_extractor.predict(hr_tile)
         
         # Train the generators
-        g_loss = adversarial_model.train_on_batch([lr_tile, hr_tile],
-                                                  [valid, image_features])
+        g_loss, _, _ = gan_model.train_on_batch([lr_tile, hr_tile],
+                                                [valid, image_features])
 
         elapsed_time = datetime.datetime.now() - start_time
         # Plot the progress
-        print(f'time: {elapsed_time} loss: {g_loss}')
+        print('time:', elapsed_time, 'g_loss:', g_loss, 'd_loss:', d_loss)
 
-        # If at save interval => save generated image samples
-        if epoch % SAMPLE_INTERVAL == 0:
+        # Save image samples to see progress.
+        if epoch % sample_interval == 0:
             save_sample_images(epoch, generator, tiles)
+
+    # TODO: improve this.
+    discriminator.save_weights('trained_networks/srgan_discriminator.h5')
+    generator.save_weights('trained_networks/srgan_generator.h5')
+
+
 
 
 def save_sample_images(epoch, generator, tiles, batch_size=2):
@@ -327,17 +335,17 @@ def save_sample_images(epoch, generator, tiles, batch_size=2):
     fake_hr = np.uint8(255 * fake_hr)
     imgs_hr = np.uint8(255 * imgs_hr)
 
-    # Save generated images and the high resolution originals
-    titles = ['Low res.', 'Generated', 'High res.']
+    # Save generated images together with originals.
+    titles = ['Low resolution', 'Generated', 'High resolution']
     fig, axs = plt.subplots(r, c)
     fig.suptitle('AMFinder SRGAN epoch {}'.format(epoch + 1), fontsize=20)
-    cnt = 0
+
     for row in range(r):
         for col, image in enumerate([imgs_lr, fake_hr, imgs_hr]):
             axs[row, col].imshow(image[row])
             axs[row, col].set_title(titles[col])
             axs[row, col].axis('off')
-        cnt += 1
+
     fig.savefig('tmp/sr-output/SR_output_{}.png'.format(epoch + 1))
     plt.close()
 
