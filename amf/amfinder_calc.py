33e0151a5f1b5c80038523375accf330979a0b9a
Edouard Evangelisti <edouard@slpc220.slcu.cam.ac.uk>
2021-05-30 10:00:58 +0100

    New module for calculating filters, conv2d outputs, and CAMs

---
+++ amf/amfinder_calc.py
@@ -0,0 +1,103 @@
+
+from copy import deepcopy 
+import numpy as np
+import tensorflow as tf
+from keras.models import Model, Input
+import tensorflow.keras.backend as K
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def compute_loss(model, input_image, filter_index):
+    activation = model(input_image)
+    # We avoid border artifacts by only involving non-border pixels in the loss.
+    filter_activation = activation[:, 2:-2, 2:-2, filter_index]
+    return tf.reduce_mean(filter_activation)
+
+
+
+@tf.function
+def gradient_ascent_step(model, img, filter_index, learning_rate=10.0):
+    with tf.GradientTape() as tape:
+        tape.watch(img)
+        loss = compute_loss(model, img, filter_index)
+    # Compute gradients.
+    grads = tape.gradient(loss, img)
+    # Normalize gradients.
+    grads = tf.math.l2_normalize(grads)
+    img += learning_rate * grads
+    return loss, img
+
+
+
+def deprocess_image(img):
+    # Normalize array: center on 0., ensure variance is 0.15
+    img -= img.mean()
+    img /= img.std() + 1e-5
+    img *= 0.15
+
+    # Center crop
+    img = img[25:-25, 25:-25, :]
+
+    # Clip to [0, 1]
+    return np.clip(img, 0, 1)
+
+
+def visualize_filter(model, filter_index):
+    # We start from a gray image with some random noise
+    img = tf.random.uniform((1, 126, 126, 3))
+    for iteration in range(30):
+        loss, img = gradient_ascent_step(model, img, filter_index)
+
+    # Decode the resulting input image
+    img = deprocess_image(img[0].numpy())
+    return loss, img
+
+
+
+def make_gradcam_heatmap(img_array, model, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+  
+    grad_model = deepcopy(model)
+    grad_model.outputs = [model.get_layer('C4').output, model.output]
+    grad_model.summary()
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        output, preds = grad_model(K.constant(img_array))
+        print(output.shape)
+        print(preds.shape)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    output = output[0]
+    heatmap = output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+def normalize(x):
+
+    return x / 255.0
+
+def get_cam(tile, model):
+    tiles = normalize(np.array([tile], np.float32))
+    heatmap = make_gradcam_heatmap(tiles, model)
+    plt.matshow(heatmap)
+    plt.show()
